7cc2025870c9f3206891ec633f181d9ad028c024
Bernhard Steubing <steubing.bernhard@gmail.com>
2014-12-20 13:33:26 +0100

    Renamed module names

--- linkedmetaprocess.py
+++ linkedmetaprocess.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from processSubsystem import ProcessSubsystem
+from metaprocess import ProcessSubsystem
 import itertools
 import numpy as np
 import networkx as nx
