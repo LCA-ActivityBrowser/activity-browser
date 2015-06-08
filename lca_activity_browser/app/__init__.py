bf4af49c006f6ae5972fa2ae6295e3897bf1dd28
Chris Mutel <cmutel@gmail.com>
2015-06-08 23:09:58 +0200

    Don't silence errors

--- lca_activity_browser/app/__init__.py
+++ lca_activity_browser/app/__init__.py
@@ -11,10 +11,17 @@ class Container(object):
 from PyQt4 import QtGui
 from .application import Application
 import sys
+import traceback
 
 
 def run_activity_browser():
     qapp = QtGui.QApplication(sys.argv)
     application = Application()
     application.show()
+
+    def exception_hook(*args):
+        print(''.join(traceback.format_exception(*args)))
+
+    sys.excepthook = exception_hook
+
     sys.exit(qapp.exec_())
