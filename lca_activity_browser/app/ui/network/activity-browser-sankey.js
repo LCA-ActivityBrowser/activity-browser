062c8d0af40fcbe03e22a1ebec0f72a676715d84
haasad <haasad@student.ethz.ch>
2017-05-29 11:11:27 +0200

    bring link to front on mouseover

--- lca_activity_browser/app/ui/network/activity-browser-sankey.js
+++ lca_activity_browser/app/ui/network/activity-browser-sankey.js
@@ -6,18 +6,32 @@ var diagram = sankeyDiagram()
     .linkColor(function(d) { return d.data.color; })
     .duration(500);
 
+//http://bl.ocks.org/eesur/4e0a69d57d3bfc8a82c2
+d3.selection.prototype.moveToFront = function() {  
+    return this.each(function(){
+        this.parentNode.appendChild(this);
+      });
+};
+
 function update_sankey(json_data){
     var json_data = JSON.parse(json_data)
     d3.select('#sankey')
         .data([json_data])
-        .call(diagram);
-}
+        .call(diagram)
+        .select('svg')
+            .selectAll('.link')
+            .on('mouseover', function(d) {
+                d3.select(this).moveToFront();
+            });
+};
 
 new QWebChannel(qt.webChannelTransport, function (channel) {
     window.bridge = channel.objects.bridge;
     window.bridge.sankey_ready.connect(update_sankey);
     window.bridge.viewer_ready();
 });
+
+
 
 diagram.on("selectLink", function(link){
     new QWebChannel(qt.webChannelTransport, function (channel) {
