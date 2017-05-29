f06405422bfca8ecdf412fbd2293aab5ff46e262
haasad <haasad@student.ethz.ch>
2017-05-30 01:16:38 +0200

    dynamic resizing of sankey
    - svg scales with window resizing via css
    - aspect ratio etc is updated with viewport whenever sankey is updated

--- lca_activity_browser/app/ui/network/activity-browser-sankey.js
+++ lca_activity_browser/app/ui/network/activity-browser-sankey.js
@@ -1,10 +1,20 @@
 var diagram = sankeyDiagram()
-    .width(1400)
-    .height(800)
-    .margins({ left: 200, right: 200, top: 10, bottom: 10 })
+    .width(windowSize()[0])
+    .height(windowSize()[1])
+    .margins({ left: 50, right: 100, top: 10, bottom: 10 })
     //.nodeTitle(function(d) { return d.data.title !== undefined ? d.data.title : d.id; })	  
     .linkColor(function(d) { return d.data.color; })
     .duration(500);
+
+function windowSize() {
+    w = window,
+    d = document,
+    e = d.documentElement,
+    g = d.getElementsByTagName('body')[0],
+    x = w.innerWidth || e.clientWidth || g.clientWidth;
+    y = w.innerHeight|| e.clientHeight|| g.clientHeight;
+    return [x,y];
+};
 
 //http://bl.ocks.org/eesur/4e0a69d57d3bfc8a82c2
 d3.selection.prototype.moveToFront = function() {  
@@ -24,12 +34,17 @@ function style(d) {
 }
 
 function update_sankey(json_data){
+    diagram.width(windowSize()[0]).height(windowSize()[1]);
     var json_data = JSON.parse(json_data)
     // update sankey with new data
     var sankey = d3.select('#sankey')
         .data([json_data])
         .call(diagram)
         .select('svg')
+            .attr("width", '100%')
+            .attr("height", '100%')
+            .attr('viewBox','0 0 '+windowSize()[0]+' '+windowSize()[1])
+            .attr('preserveAspectRatio','none')
     // move mouseover element to front and display tooltip 
     sankey.selectAll('.link')
         .on('mouseover', function(d) {
@@ -54,12 +69,12 @@ function update_sankey(json_data){
         .style('stroke-width', function(d) { return style(d) === 'process' ? '5px' : '1px'; });
 };
 
+
 new QWebChannel(qt.webChannelTransport, function (channel) {
     window.bridge = channel.objects.bridge;
     window.bridge.sankey_ready.connect(update_sankey);
     window.bridge.viewer_ready();
 });
-
 
 
 diagram.on("selectLink", function(link){
