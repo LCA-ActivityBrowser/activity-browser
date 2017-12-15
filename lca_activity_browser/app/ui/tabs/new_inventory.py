7df1344034eecd131d3c5cf389f8a26ed3f44274
bsteubing <steubing.bernhard@gmail.com>
2017-12-15 18:28:32 +0100

    still playing around with size policies...

--- lca_activity_browser/app/ui/tabs/new_inventory.py
+++ lca_activity_browser/app/ui/tabs/new_inventory.py
@@ -21,12 +21,27 @@ class NewInventoryTab(QtWidgets.QWidget):
         self.activities_widget = ActivitiesWidget()
         self.flows_widget = BiosphereFlowsWidget()
 
+        self.splitter = QtWidgets.QSplitter(QtCore.Qt.Vertical)
+        # splitter_layout = QtWidgets.QVBoxLayout()
+        # splitter_layout.setAlignment(QtCore.Qt.AlignTop)
+
+        # self.splitter.addWidget(self.projects_widget)
+        self.splitter.addWidget(self.databases_widget)
+        self.splitter.addWidget(self.activities_widget)
+        self.splitter.addWidget(self.flows_widget)
+
+        # splitter_layout.addWidget(self.databases_widget)
+        # splitter_layout.addWidget(self.activities_widget)
+        # splitter_layout.addWidget(self.flows_widget)
+        # self.splitter.setLayout(splitter_layout)
+
         self.overall_layout = QtWidgets.QVBoxLayout()
         self.overall_layout.setAlignment(QtCore.Qt.AlignTop)
         self.overall_layout.addWidget(self.projects_widget)
-        self.overall_layout.addWidget(self.databases_widget)
-        self.overall_layout.addWidget(self.activities_widget)
-        self.overall_layout.addWidget(self.flows_widget)
+        # self.overall_layout.addWidget(self.databases_widget)
+        # self.overall_layout.addWidget(self.activities_widget)
+        # self.overall_layout.addWidget(self.flows_widget)
+        self.overall_layout.addWidget(self.splitter)
         self.setLayout(self.overall_layout)
 
         self.activities_widget.hide()
@@ -38,12 +53,13 @@ class NewInventoryTab(QtWidgets.QWidget):
         signals.database_selected.connect(self.update_widgets)
 
     def update_widgets(self):
-        if self.activities_widget.activities_table.rowCount() == 0:
+        # pass
+        # self.updateGeometry()
+        if self.activities_widget.table.rowCount() == 0:
             self.activities_widget.hide()
         else:
             self.activities_widget.show()
-
-        if self.flows_widget.flows_table.rowCount() == 0:
+        if self.flows_widget.table.rowCount() == 0:
             self.flows_widget.hide()
         else:
             self.flows_widget.show()
@@ -64,17 +80,38 @@ class ProjectsWidget(QtWidgets.QWidget):
         self.h_layout.addWidget(self.copy_project_button)
         self.h_layout.addWidget(self.delete_project_button)
         self.setLayout(self.h_layout)
-
+        self.setSizePolicy(QtWidgets.QSizePolicy(
+            QtWidgets.QSizePolicy.Maximum,
+            QtWidgets.QSizePolicy.Maximum)
+        )
 
 class DatabaseWidget(QtWidgets.QWidget):
     def __init__(self):
         super(DatabaseWidget, self).__init__()
-        self.databases_table = DatabasesTable()
+
+        self.table = DatabasesTable()
+
+        # Header widget
+        self.header_widget = QtWidgets.QWidget()
+        header_layout = QtWidgets.QHBoxLayout()
+        header_layout.addWidget(header('Databases'))
+        self.header_widget.setLayout(header_layout)
+
+        # Overall Layout
         v_layout = QtWidgets.QVBoxLayout()
         v_layout.setAlignment(QtCore.Qt.AlignTop)
-        v_layout.addWidget(header('Databases'))
-        v_layout.addWidget(self.databases_table)
+        v_layout.addWidget(self.header_widget)
+        v_layout.addWidget(self.table)
         self.setLayout(v_layout)
+
+        self.header_widget.setSizePolicy(QtWidgets.QSizePolicy(
+            QtWidgets.QSizePolicy.Maximum,
+            QtWidgets.QSizePolicy.Maximum)
+        )
+        self.table.setSizePolicy(QtWidgets.QSizePolicy(
+            QtWidgets.QSizePolicy.Minimum,
+            QtWidgets.QSizePolicy.Preferred)
+        )
         self.setSizePolicy(QtWidgets.QSizePolicy(
             QtWidgets.QSizePolicy.Minimum,
             QtWidgets.QSizePolicy.Maximum)
@@ -84,20 +121,55 @@ class DatabaseWidget(QtWidgets.QWidget):
 class ActivitiesWidget(QtWidgets.QWidget):
     def __init__(self):
         super(ActivitiesWidget, self).__init__()
-        self.activities_table = ActivitiesTable()
+
+        self.table = ActivitiesTable()
+
+        # Header widget
+        self.header_widget = QtWidgets.QWidget()
+        header_layout = QtWidgets.QHBoxLayout()
+        header_layout.addWidget(header('Activities'))
+        self.header_widget.setLayout(header_layout)
+
+        # Overall Layout
         v_layout = QtWidgets.QVBoxLayout()
         v_layout.setAlignment(QtCore.Qt.AlignTop)
-        v_layout.addWidget(header('Activities'))
-        v_layout.addWidget(self.activities_table)
+        v_layout.addWidget(self.header_widget)
+        v_layout.addWidget(self.table)
         self.setLayout(v_layout)
 
+        self.header_widget.setSizePolicy(QtWidgets.QSizePolicy(
+            QtWidgets.QSizePolicy.Maximum,
+            QtWidgets.QSizePolicy.Maximum)
+        )
+        self.table.setSizePolicy(QtWidgets.QSizePolicy(
+            QtWidgets.QSizePolicy.Minimum,
+            QtWidgets.QSizePolicy.Preferred)
+        )
 
 class BiosphereFlowsWidget(QtWidgets.QWidget):
     def __init__(self):
         super(BiosphereFlowsWidget, self).__init__()
-        self.flows_table = BiosphereFlowsTable()
+
+        self.table = BiosphereFlowsTable()
+
+        # Header widget
+        self.header_widget = QtWidgets.QWidget()
+        header_layout = QtWidgets.QHBoxLayout()
+        header_layout.addWidget(header('Biosphere Flows'))
+        self.header_widget.setLayout(header_layout)
+
+        # Overall Layout
         v_layout = QtWidgets.QVBoxLayout()
         v_layout.setAlignment(QtCore.Qt.AlignTop)
-        v_layout.addWidget(header('Biosphere Flows'))
-        v_layout.addWidget(self.flows_table)
+        v_layout.addWidget(self.header_widget)
+        v_layout.addWidget(self.table)
         self.setLayout(v_layout)
+
+        self.header_widget.setSizePolicy(QtWidgets.QSizePolicy(
+            QtWidgets.QSizePolicy.Maximum,
+            QtWidgets.QSizePolicy.Maximum)
+        )
+        self.table.setSizePolicy(QtWidgets.QSizePolicy(
+            QtWidgets.QSizePolicy.Minimum,
+            QtWidgets.QSizePolicy.Preferred)
+        )
