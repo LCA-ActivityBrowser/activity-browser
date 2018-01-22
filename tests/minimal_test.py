55d104d544ded8ad68038f4cb8b811d9a6534d29
haasad <haasad@student.ethz.ch>
2018-01-22 19:34:23 +0100

    minimal travis test where AB is run as GUI (#88)
    * xvfb-run with 10s timout, first trial
    
    * start xvfb manually
    
    * display xvfd error message
    
    * new try with installing drivers
    
    * trying some other drivers
    
    * temporarily pin bw2io to 0.6.RC3 for travis until fixed upstream
    
    * try tricks from pytest-qt, ie. maybe a window manager was missing
    
    * using tricks from astropy/ci-helpers
    
    * minimal test with pytest-qt
    
    * wrong package name for pytest-qt
    
    * maybe window manager is not even required
    
    * clean up

---
+++ tests/minimal_test.py
@@ -0,0 +1,14 @@
+import os
+import sys
+
+from pytestqt import qtbot
+
+p = os.path.abspath(os.path.join(os.path.split(__file__)[0], '..'))
+sys.path.append(p)
+
+from lca_activity_browser.app import Application
+
+
+def test_minimal(qtbot):
+    app = Application()
+    qtbot.addWidget(app)
