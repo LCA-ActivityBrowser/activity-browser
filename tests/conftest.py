1c56dfc79bd48a75a03f23597974d436bc47e1c5
haasad <haasad@student.ethz.ch>
2018-01-25 16:56:32 +0100

    test for changing projects and renaming the test projects

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,11 +3,14 @@ import time
 
 import pytest
 
+import brightway2 as bw
 from activity_browser import Application
 
 
-@pytest.fixture(scope='module')
+@pytest.fixture(scope='session')
 def ab_app():
+    if 'pytest_project' in bw.projects:
+        bw.projects.delete_project('pytest_project', delete_dir=True)
     application = Application()
     application.show()
     time.sleep(1)
--- tests/test_projects.py
+++ tests/test_projects.py
@@ -5,23 +5,31 @@ from PyQt5 import QtCore
 from activity_browser.app.controller import Controller
 
 
-def test_default_project():
-    assert bw.projects.current == 'default'
-
-
 def test_new_project(qtbot, mock, ab_app):
     qtbot.waitForWindowShown(ab_app.main_window)
-    mock.patch.object(Controller, 'get_new_project_name', return_value='test_project')
+    mock.patch.object(Controller, 'get_new_project_name', return_value='pytest_project_del')
     qtbot.mouseClick(
         ab_app.main_window.right_panel.inventory_tab.projects_widget.new_project_button,
         QtCore.Qt.LeftButton
     )
-    assert bw.projects.current == 'test_project'
+    assert bw.projects.current == 'pytest_project_del'
+
+
+def test_change_project(qtbot, ab_app):
+    qtbot.waitForWindowShown(ab_app.main_window)
+    assert bw.projects.current == 'pytest_project_del'
+    combobox = ab_app.main_window.right_panel.inventory_tab.projects_widget.projects_list
+    assert 'default' in bw.projects
+    assert 'default' in combobox.project_names
+    combobox.activated.emit(combobox.project_names.index('default'))
+    assert bw.projects.current == 'default'
+    combobox.activated.emit(combobox.project_names.index('pytest_project_del'))
+    assert bw.projects.current == 'pytest_project_del'
 
 
 def test_delete_project(qtbot, mock, ab_app):
     qtbot.waitForWindowShown(ab_app.main_window)
-    assert bw.projects.current == 'test_project'
+    assert bw.projects.current == 'pytest_project_del'
     mock.patch.object(Controller, 'confirm_project_deletion', return_value=True)
     qtbot.mouseClick(
         ab_app.main_window.right_panel.inventory_tab.projects_widget.delete_project_button,
