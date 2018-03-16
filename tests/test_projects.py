18e251b9b95bb63b61c7bb8846fd2003e71095c9
haasad <haasad@student.ethz.ch>
2018-03-16 14:58:14 +0100

    update tests to new renamed tabs

--- tests/test_projects.py
+++ tests/test_projects.py
@@ -9,7 +9,7 @@ def test_new_project(qtbot, mock, ab_app):
     qtbot.waitForWindowShown(ab_app.main_window)
     mock.patch.object(Controller, 'get_new_project_name', return_value='pytest_project_del')
     qtbot.mouseClick(
-        ab_app.main_window.right_panel.inventory_tab.projects_widget.new_project_button,
+        ab_app.main_window.right_panel.project_tab.projects_widget.new_project_button,
         QtCore.Qt.LeftButton
     )
     assert bw.projects.current == 'pytest_project_del'
@@ -18,7 +18,7 @@ def test_new_project(qtbot, mock, ab_app):
 def test_change_project(qtbot, ab_app):
     qtbot.waitForWindowShown(ab_app.main_window)
     assert bw.projects.current == 'pytest_project_del'
-    combobox = ab_app.main_window.right_panel.inventory_tab.projects_widget.projects_list
+    combobox = ab_app.main_window.right_panel.project_tab.projects_widget.projects_list
     assert 'default' in bw.projects
     assert 'default' in combobox.project_names
     combobox.activated.emit(combobox.project_names.index('default'))
@@ -32,7 +32,7 @@ def test_delete_project(qtbot, mock, ab_app):
     assert bw.projects.current == 'pytest_project_del'
     mock.patch.object(Controller, 'confirm_project_deletion', return_value=True)
     qtbot.mouseClick(
-        ab_app.main_window.right_panel.inventory_tab.projects_widget.delete_project_button,
+        ab_app.main_window.right_panel.project_tab.projects_widget.delete_project_button,
         QtCore.Qt.LeftButton
     )
     assert bw.projects.current == 'default'
