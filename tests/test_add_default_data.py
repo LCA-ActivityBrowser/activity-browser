4e55b6d752f14a87c9ff83515f44614b390a7a66
haasad <haasad@student.ethz.ch>
2018-04-21 14:06:02 +0200

    also change dialogue/dialog/diaglogue in tests

--- tests/test_add_default_data.py
+++ tests/test_add_default_data.py
@@ -10,7 +10,7 @@ from activity_browser.app.ui.wizards.db_import_wizard import import_signals
 def test_add_default_data(qtbot, mock, ab_app):
     assert bw.projects.current == 'default'
     qtbot.waitForWindowShown(ab_app.main_window)
-    mock.patch.object(Controller, 'get_new_project_name_diaglogue', return_value='pytest_project')
+    mock.patch.object(Controller, 'get_new_project_name_dialog', return_value='pytest_project')
     qtbot.mouseClick(
         ab_app.main_window.right_panel.project_tab.projects_widget.new_project_button,
         QtCore.Qt.LeftButton
--- tests/test_projects.py
+++ tests/test_projects.py
@@ -8,7 +8,7 @@ from activity_browser.app.controller import Controller
 def test_new_project(qtbot, mock, ab_app):
     qtbot.waitForWindowShown(ab_app.main_window)
     mock.patch.object(
-        Controller, 'get_new_project_name_diaglogue', return_value='pytest_project_del'
+        Controller, 'get_new_project_name_dialog', return_value='pytest_project_del'
     )
     qtbot.mouseClick(
         ab_app.main_window.right_panel.project_tab.projects_widget.new_project_button,
@@ -32,7 +32,7 @@ def test_change_project(qtbot, ab_app):
 def test_delete_project(qtbot, mock, ab_app):
     qtbot.waitForWindowShown(ab_app.main_window)
     assert bw.projects.current == 'pytest_project_del'
-    mock.patch.object(Controller, 'confirm_project_deletion_dialogue', return_value=True)
+    mock.patch.object(Controller, 'confirm_project_deletion_dialog', return_value=True)
     qtbot.mouseClick(
         ab_app.main_window.right_panel.project_tab.projects_widget.delete_project_button,
         QtCore.Qt.LeftButton
