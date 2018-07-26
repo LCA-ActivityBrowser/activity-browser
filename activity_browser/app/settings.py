6df40ecb2a311580431c6451d457e9f728e97004
haasad <haasad@student.ethz.ch>
2018-07-26 14:08:10 +0200

    use appdirs to save user data

--- activity_browser/app/settings.py
+++ activity_browser/app/settings.py
@@ -1,17 +1,34 @@
 # -*- coding: utf-8 -*-
 import os
 import json
+import shutil
+
+import appdirs
 
 from .. import PACKAGE_DIRECTORY
 
 
 class ABSettings():
     def __init__(self):
-        self.settings_file = os.path.join(PACKAGE_DIRECTORY, 'ABsettings.json')
+        ab_dir = appdirs.AppDirs('ActivityBrowser', 'ABData')
+        self.data_dir = ab_dir.user_data_dir
+        if not os.path.isdir(self.data_dir):
+            os.mkdir(self.data_dir)
+        self.settings_file = os.path.join(self.data_dir, 'ABsettings.json')
+        self.move_old_settings()
         if os.path.isfile(self.settings_file):
             self.load_settings()
         else:
             self.settings = {}
+
+    def move_old_settings(self):
+        if not os.path.exists(self.settings_file):
+            old_settings = os.path.join(PACKAGE_DIRECTORY, 'ABsettings.json')
+            print(old_settings)
+            print(os.path.exists(old_settings))
+            if os.path.exists(old_settings):
+                print('called')
+                shutil.copyfile(old_settings, self.settings_file)
 
     def load_settings(self):
         with open(self.settings_file, 'r') as infile:
