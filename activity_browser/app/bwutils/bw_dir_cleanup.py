266c61916a4f12ec00364d5d8783ca3c58726c7f
haasad <haasad@student.ethz.ch>
2018-10-12 17:06:58 +0200

    remove redundant file

--- activity_browser/app/bwutils/bw_dir_cleanup.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-import brightway2 as bw
-
-
-def cleanup():
-    n_dir = bw.projects.purge_deleted_directories()
-    print('Deleted {} unused project directiories!'.format(n_dir))
