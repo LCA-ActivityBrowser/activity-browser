f352b672c0c9e1d8715ed323cf02bee63c517af6
bsteubing <steubing.bernhard@gmail.com>
2019-03-12 19:36:11 +0100

    Update of welcome tab

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ for dirpath, dirnames, filenames in os.walk('activity_browser'):
 
 setup(
     name='activity-browser',
-    version="2.2.5",
+    version="2.3.0",
     packages=packages,
     include_package_data=True,
     author="Bernhard Steubing",
@@ -26,7 +26,7 @@ setup(
     install_requires=[], # dependency management in conda recipe
     url="https://github.com/LCA-ActivityBrowser/activity-browser",
     long_description=open('README.md').read(),
-    description=('A graphical user interface for brightway2'),
+    description='A graphical user interface for brightway2',
     entry_points={
         'console_scripts': [
             'activity-browser = activity_browser.app:run_activity_browser',
