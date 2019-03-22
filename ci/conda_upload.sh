42eb5bc3b831dd70e80a629c6e46b596cfeee448
bsteubing <steubing.bernhard@gmail.com>
2019-03-22 14:56:45 +0100

    conda_build.sh

---
+++ ci/conda_upload.sh
@@ -0,0 +1,11 @@
+# Only need to change these two variables
+PKG_NAME=activity-browser-dev
+USER=bsteubing
+
+mkdir ~/conda-bld
+conda config --set anaconda_upload no
+export CONDA_BLD_PATH=~/conda-bld
+export VERSION=`date +%Y.%m.%d`
+conda build . --old-build-string
+ls $CONDA_BLD_PATH/noarch/
+anaconda -t $CONDA_UPLOAD_TOKEN upload -u $USER -l nightly $CONDA_BLD_PATH/noarch/$PKG_NAME-$VERSION-py_0.tar.bz2 --force
