ab835b2ede74e349cd6a5fb167250ed696c8cff2
bsteubing <steubing.bernhard@gmail.com>
2019-03-22 16:19:00 +0100

    conda upload script

--- ci/conda_upload.sh
+++ ci/conda_upload.sh
@@ -6,6 +6,12 @@ mkdir ~/conda-bld
 conda config --set anaconda_upload no
 export CONDA_BLD_PATH=~/conda-bld
 export VERSION=`date +%Y.%m.%d`
+
+echo "RUNNING BUILD"
 conda build . --old-build-string
+echo "BUILD FINISHED"
+
 ls $CONDA_BLD_PATH/noarch/
+
+echo "UPLOADING BUILD: $USER"
 anaconda -t $CONDA_UPLOAD_TOKEN upload -u $USER -l nightly $CONDA_BLD_PATH/noarch/$PKG_NAME-$VERSION-py_0.tar.bz2 --force
