c3b1226e6d15fab6a0942d684b0f28c356264b3c
Daniel de Koning <dgdekoning@users.noreply.github.com>
2019-04-12 14:18:08 +0200

    Refactor travis pipeline (#233)
    * [skip appveyor] Implement deploy step in travis script and dependent scripts
    
    * [skip ci] Small alterations to manual build recipe
    
    * [skip ci] Remove unused scripts to avoid confusion
    
    * [skip appveyor] Add missing install step for required packages
    
    * [skip appveyor] Return ./ to conda build paths, return pydist env variable to osx
    
    * [skip appveyor] Correct environ selection with some default values
    
    * [skip appveyor] Undoing some more of my changes, should fix osx and linux both
    
    * [skip appveyor] Revert some more changes
    
    * [skip appveyor] Revert combining of pytest calls in linux env

--- ci/conda_upload.sh
+++ ci/conda_upload.sh
@@ -1,18 +1,36 @@
-# Only need to change these two variables
-PKG_NAME=activity-browser-dev
-USER=bsteubing
+#!/usr/bin/env bash
+# Script for building and uploading the dev and/or stable packages to conda
 
-mkdir ~/conda-bld
-conda config --set anaconda_upload no
-export CONDA_BLD_PATH=~/conda-bld
-export VERSION=`date +%Y.%m.%d`
+# Only upload from linux, and use different variables for dev and stable builds
+if [ "$TRAVIS_OS_NAME" == "linux" ]; then
+    USER=bsteubing
+    mkdir -p ~/conda-bld
+    conda config --set anaconda_upload no
+    export CONDA_BLD_PATH=~/conda-bld
 
-echo "RUNNING BUILD"
-conda build . --old-build-string
-echo "BUILD FINISHED"
+    # Check if TRAVIS_TAG variable is set or empty.
+    if [ -z "$TRAVIS_TAG" ]; then
+        export PKG_NAME="activity-browser-dev"
+        export VERSION=`date +%Y.%m.%d`
+        BUILD_ARGS="--old-build-string"
+        UPLOAD_ARGS="--force"
+    else
+        export PKG_NAME="activity-browser"
+        export VERSION="$TRAVIS_TAG"
+        BUILD_ARGS=""
+        UPLOAD_ARGS=""
+    fi
 
-ls $CONDA_BLD_PATH/noarch/
+    conda install -q conda-build anaconda-client conda-verify
+    echo "RUNNING BUILD"
+    conda build ./travis/recipe $BUILD_ARGS
+    echo "BUILD FINISHED"
 
+    ls $CONDA_BLD_PATH/noarch/
 
-echo "UPLOADING BUILD: $USER"
-anaconda -t $CONDA_UPLOAD_TOKEN upload -u $USER -l main $CONDA_BLD_PATH/noarch/$PKG_NAME-$VERSION-py_0.tar.bz2 --force
+    echo "UPLOADING BUILD: $USER"
+    anaconda -t $CONDA_UPLOAD_TOKEN upload -u $USER -l main $CONDA_BLD_PATH/noarch/$PKG_NAME-$VERSION-py_0.tar.bz2 $UPLOAD_ARGS
+    echo "BUILD UPLOADED: $USER"
+else
+    echo "No uploads from MacOS"
+fi
