fb42791f5cbd6a377b8e6145036151e98f9ee212
dgdekoning <daniel.de.koning@gmail.com>
2019-07-08 14:55:41 +0200

    Add delegates to allow editing in model-views

---
+++ activity_browser/app/ui/tables/delegates.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+from PyQt5.QtGui import QDoubleValidator, QRegExpValidator
+from PyQt5.QtWidgets import QItemDelegate, QLineEdit
+
+
+class FloatDelegate(QItemDelegate):
+    """ For validating entered float values
+    """
+    def __init__(self, parent):
+        super().__init__(parent)
+
+    def createEditor(self, parent, option, index):
+        editor = QLineEdit(parent)
+        editor.setValidator(QDoubleValidator())
+        return editor
+
+
+class StringDelegate(QItemDelegate):
+    """ For validating entered string values
+    """
+    def __init__(self, parent, regex=None):
+        super().__init__(parent)
+        self.regex = regex
+
+    def createEditor(self, parent, option, index):
+        editor = QLineEdit(parent)
+        if self.regex:
+            editor.setValidator(QRegExpValidator(self.regex))
+        return editor
