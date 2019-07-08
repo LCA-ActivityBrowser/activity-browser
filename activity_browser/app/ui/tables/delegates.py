6b7b5dd9ca1dda3e6a470dc597c74c94fabf782c
dgdekoning <daniel.de.koning@gmail.com>
2019-07-08 17:22:30 +0200

    Delegates now prefill edit field and handle input

--- activity_browser/app/ui/tables/delegates.py
+++ activity_browser/app/ui/tables/delegates.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
+from PyQt5.QtCore import QModelIndex, Qt, QAbstractItemModel
 from PyQt5.QtGui import QDoubleValidator, QRegExpValidator
-from PyQt5.QtWidgets import QItemDelegate, QLineEdit
+from PyQt5.QtWidgets import QItemDelegate, QLineEdit, QTextEdit, QWidget
 
 
 class FloatDelegate(QItemDelegate):
-    """ For validating entered float values
+    """ For managing and validating entered float values
     """
     def __init__(self, parent=None):
         super().__init__(parent)
@@ -14,16 +15,39 @@ class FloatDelegate(QItemDelegate):
         editor.setValidator(QDoubleValidator())
         return editor
 
+    def setEditorData(self, editor: QLineEdit, index: QModelIndex):
+        """ Populate the editor with data if editing an existing field.
+        """
+        value = str(index.data(Qt.DisplayRole))
+        editor.setText(value)
+
+    def setModelData(self, editor: QLineEdit, model: QAbstractItemModel,
+                     index: QModelIndex):
+        """ Take the editor, read the given value and set it in the model
+        """
+        value = float(editor.text())
+        model.setData(index, value, Qt.EditRole)
+
 
 class StringDelegate(QItemDelegate):
-    """ For validating entered string values
+    """ For managing and validating entered string values
     """
-    def __init__(self, parent=None, regex=None):
+    def __init__(self, parent=None):
         super().__init__(parent)
-        self.regex = regex
 
     def createEditor(self, parent, option, index):
         editor = QLineEdit(parent)
-        if self.regex:
-            editor.setValidator(QRegExpValidator(self.regex))
         return editor
+
+    def setEditorData(self, editor: QLineEdit, index: QModelIndex):
+        """ Populate the editor with data if editing an existing field.
+        """
+        value = str(index.data(Qt.DisplayRole))
+        editor.setText(value)
+
+    def setModelData(self, editor: QLineEdit, model: QAbstractItemModel,
+                     index: QModelIndex):
+        """ Take the editor, read the given value and set it in the model
+        """
+        value = editor.text()
+        model.setData(index, value, Qt.EditRole)
