5509c87de017df5f37c5d1633ed233400bf3405f
dgdekoning <daniel.de.koning@gmail.com>
2019-07-10 16:35:44 +0200

    Add bw database delegate and sort imports

--- activity_browser/app/ui/tables/delegates.py
+++ activity_browser/app/ui/tables/delegates.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
-from PyQt5.QtCore import QModelIndex, Qt, QAbstractItemModel
+import brightway2 as bw
+from PyQt5.QtCore import QAbstractItemModel, QModelIndex, Qt
 from PyQt5.QtGui import QDoubleValidator, QRegExpValidator
-from PyQt5.QtWidgets import QItemDelegate, QLineEdit, QTextEdit, QWidget
+from PyQt5.QtWidgets import (QComboBox, QItemDelegate, QLineEdit, QTextEdit,
+                             QWidget)
 
 
 class FloatDelegate(QItemDelegate):
@@ -50,4 +52,30 @@ class StringDelegate(QItemDelegate):
         """ Take the editor, read the given value and set it in the model
         """
         value = editor.text()
+        model.setData(index, value, Qt.EditRole)
+
+
+class DatabaseDelegate(QItemDelegate):
+    """ Nearly the same as the string delegate, but presents as
+    a combobox menu containing the databases of the current project
+    """
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+    def createEditor(self, parent, option, index):
+        editor = QComboBox(parent)
+        editor.insertItems(0, bw.databases.list)
+        return editor
+
+    def setEditorData(self, editor: QComboBox, index: QModelIndex):
+        """ Populate the editor with data if editing an existing field.
+        """
+        value = str(index.data(Qt.DisplayRole))
+        editor.setCurrentText(value)
+
+    def setModelData(self, editor: QComboBox, model: QAbstractItemModel,
+                     index: QModelIndex):
+        """ Take the editor, read the given value and set it in the model
+        """
+        value = editor.currentText()
         model.setData(index, value, Qt.EditRole)
