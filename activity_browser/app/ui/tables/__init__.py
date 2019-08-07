e85f2d16ebb2f0dcf0561f1328f3215d6154d396
dgdekoning <daniel.de.koning@gmail.com>
2019-08-07 15:36:46 +0200

    Sort imports and keep base table classes inside tables directory

--- activity_browser/app/ui/tables/__init__.py
+++ activity_browser/app/ui/tables/__init__.py
@@ -1,14 +1,8 @@
 # -*- coding: utf-8 -*-
-from .LCA_setup import (
-    CSActivityTable,
-    CSList,
-    CSMethodsTable,
-)
-from .inventory import DatabasesTable, ActivitiesBiosphereTable
-from .activity import ExchangeTable  # ProductTable
+from .activity import ExchangeTable
 from .history import ActivitiesHistoryTable
 from .impact_categories import CFTable, MethodsTable
-from .lca_results import LCAResultsTable, InventoryTable, ContributionTable
-from .projects import ProjectTable, ProjectListWidget
-from .table import ABTableWidget, ABTableItem
-from .views import ABDataFrameView
+from .inventory import ActivitiesBiosphereTable, DatabasesTable
+from .lca_results import ContributionTable, InventoryTable, LCAResultsTable
+from .LCA_setup import CSActivityTable, CSList, CSMethodsTable
+from .projects import ProjectListWidget, ProjectTable
