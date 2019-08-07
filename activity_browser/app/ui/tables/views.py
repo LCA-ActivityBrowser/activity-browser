f80dd1a931da8327b3b25e67049d17f6297ac5c9
dgdekoning <daniel.de.koning@gmail.com>
2019-08-07 15:35:18 +0200

    Add simple-copy and editable view classes, improve documentation

--- activity_browser/app/ui/tables/views.py
+++ activity_browser/app/ui/tables/views.py
@@ -1,17 +1,25 @@
 # -*- coding: utf-8 -*-
 import os
+from functools import wraps
 
 from PyQt5.QtCore import QSize, QSortFilterProxyModel, Qt, pyqtSlot
 from PyQt5.QtWidgets import QFileDialog, QTableView
 
-from activity_browser.app.settings import ABSettings
+from activity_browser.app.settings import ab_settings
 
-from ..style import style_item
-from .models import DragPandasModel, PandasModel
+from .models import (DragPandasModel, EditableDragPandasModel,
+                     EditablePandasModel, PandasModel,
+                     SimpleCopyDragPandasModel, SimpleCopyPandasModel)
 
 
 class ABDataFrameView(QTableView):
-    def __init__(self, parent=None, maxheight=None, *args, **kwargs):
+    """ Base class for showing pandas dataframe objects as tables.
+    """
+    ALL_FILTER = "All Files (*.*)"
+    CSV_FILTER = "CSV (*.csv);; All Files (*.*)"
+    EXCEL_FILTER = "Excel (*.xlsx);; All Files (*.*)"
+
+    def __init__(self, parent=None):
         super().__init__(parent)
         self.setVerticalScrollMode(1)
         self.setHorizontalScrollMode(1)
@@ -33,6 +41,12 @@ class ABDataFrameView(QTableView):
 
     @classmethod
     def decorated_sync(cls, sync):
+        """ Syncs the data from the dataframe into the table view.
+
+        Uses either of the PandasModel classes depending if the class is
+        'drag-enabled'.
+        """
+        @wraps(sync)
         def wrapper(self, *args, **kwargs):
             sync(self, *args, **kwargs)
 
@@ -40,7 +54,8 @@ class ABDataFrameView(QTableView):
                 self.model = DragPandasModel(self.dataframe)
             else:
                 self.model = PandasModel(self.dataframe)
-            self.proxy_model = QSortFilterProxyModel()  # see: http://doc.qt.io/qt-5/qsortfilterproxymodel.html#details
+            # See: http://doc.qt.io/qt-5/qsortfilterproxymodel.html#details
+            self.proxy_model = QSortFilterProxyModel()
             self.proxy_model.setSourceModel(self.model)
             self.proxy_model.setSortCaseSensitivity(Qt.CaseInsensitive)
             self.setModel(self.proxy_model)
@@ -48,36 +63,50 @@ class ABDataFrameView(QTableView):
 
         return wrapper
 
-    def get_source_index(self, proxy_index):
-        """Returns the index of the original model from a proxymodel index.
-        This way data from the self._dataframe can be obtained correctly."""
+    @staticmethod
+    def get_source_index(proxy_index):
+        """ Returns the index of the original model from a proxymodel index.
+
+        This way data from the self._dataframe can be obtained correctly.
+        """
         model = proxy_index.model()
         if hasattr(model, 'mapToSource'):
             # We are a proxy model
             source_index = model.mapToSource(proxy_index)
-        return source_index
+            return source_index
+        return None
 
     def to_clipboard(self):
+        """ Copy dataframe to clipboard
+        """
         self.dataframe.to_clipboard()
 
-    def savefilepath(self, default_file_name: str, filter="All Files (*.*)"):
+    def savefilepath(self, default_file_name: str, file_filter: str=None):
+        """ Construct and return default path where data is stored
+
+        Uses the application directory for AB
+        """
         filepath, _ = QFileDialog.getSaveFileName(
             self,
             caption='Choose location to save lca results',
-            directory=os.path.join(ABSettings.data_dir, default_file_name),
-            filter=filter,
+            directory=os.path.join(ab_settings.data_dir, default_file_name),
+            filter=file_filter or self.ALL_FILTER,
         )
         return filepath
 
     def to_csv(self):
-        filepath = self.savefilepath(default_file_name=self.table_name, filter="CSV (*.csv);; All Files (*.*)")
+        """ Save the dataframe data to a CSV file.
+        """
+        filepath = self.savefilepath(self.table_name, file_filter=self.CSV_FILTER)
         if filepath:
             if not filepath.endswith('.csv'):
                 filepath += '.csv'
             self.dataframe.to_csv(filepath)
 
     def to_excel(self):
-        filepath = self.savefilepath(default_file_name=self.table_name, filter="Excel (*.xlsx);; All Files (*.*)")
+        """ Save the dataframe data to an excel file.
+        """
+        filepath = self.savefilepath(self.table_name, file_filter=self.EXCEL_FILTER)
         if filepath:
             if not filepath.endswith('.xlsx'):
                 filepath += '.xlsx'
@@ -85,6 +114,10 @@ class ABDataFrameView(QTableView):
 
     @pyqtSlot()
     def keyPressEvent(self, e):
+        """ Allow user to copy selected data from the table
+
+        NOTE: by default, the table headers (column names) are also copied.
+        """
         if e.modifiers() and Qt.ControlModifier:
             if e.key() == Qt.Key_C:  # copy
                 selection = [self.get_source_index(pindex) for pindex in self.selectedIndexes()]
@@ -92,4 +125,55 @@ class ABDataFrameView(QTableView):
                 columns = [index.column() for index in selection]
                 rows = sorted(set(rows), key=rows.index)
                 columns = sorted(set(columns), key=columns.index)
-                self.model._dataframe.iloc[rows, columns].to_clipboard(index=False)  # includes headers
+                self.model.to_clipboard(rows, columns)
+
+
+class ABDataFrameSimpleCopy(ABDataFrameView):
+    """ A view-only class which copies values without including headers
+    """
+    @classmethod
+    def decorated_sync(cls, sync):
+        """ Syncs the data from the dataframe into simple copy models
+        """
+        @wraps(sync)
+        def wrapper(self, *args, **kwargs):
+            sync(self, *args, **kwargs)
+
+            if hasattr(self, 'drag_model'):
+                self.model = SimpleCopyDragPandasModel(self.dataframe)
+            else:
+                self.model = SimpleCopyPandasModel(self.dataframe)
+            self.proxy_model = QSortFilterProxyModel()
+            self.proxy_model.setSourceModel(self.model)
+            self.proxy_model.setSortCaseSensitivity(Qt.CaseInsensitive)
+            self.setModel(self.proxy_model)
+            self.setMaximumHeight(self.get_max_height())
+
+        return wrapper
+
+
+class ABDataFrameEdit(ABDataFrameView):
+    """ Inherit from view class but override decorated_sync to use
+    editable models.
+    """
+    @classmethod
+    def decorated_sync(cls, sync):
+        """ Syncs the data from the dataframe into editable models
+        """
+        @wraps(sync)
+        def wrapper(self, *args, **kwargs):
+            sync(self, *args, **kwargs)
+
+            if hasattr(self, 'drag_model'):
+                self.model = EditableDragPandasModel(self.dataframe)
+            else:
+                self.model = EditablePandasModel(self.dataframe)
+            self.proxy_model = QSortFilterProxyModel()  # see: http://doc.qt.io/qt-5/qsortfilterproxymodel.html#details
+            self.proxy_model.setSourceModel(self.model)
+            self.proxy_model.setSortCaseSensitivity(Qt.CaseInsensitive)
+            self.setModel(self.proxy_model)
+            self.setMaximumHeight(self.get_max_height())
+            self.resizeColumnsToContents()
+            self.resizeRowsToContents()
+
+        return wrapper
