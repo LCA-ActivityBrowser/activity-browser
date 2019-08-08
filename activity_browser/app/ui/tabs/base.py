b53d1efa36ad95e2d43b373a4af41011b6666a59
dgdekoning <daniel.de.koning@gmail.com>
2019-08-08 12:17:57 +0200

    Add simple base QWidget class with explanation slot

---
+++ activity_browser/app/ui/tabs/base.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+from PyQt5.QtCore import pyqtSlot
+from PyQt5.QtWidgets import QMessageBox, QWidget
+
+
+class BaseRightTab(QWidget):
+    """ Extremely basic widget, to be created and used inside a QTabWidget
+
+    Contains an `explanation` slot which presents the `explain_text` inside a popup
+    """
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.explain_text = "I explain what happens here"
+
+    def _connect_signals(self):
+        """ Used by child classes to wire up specific functionality to signals
+
+        Should only be used during init
+        """
+        raise NotImplementedError
+
+    def _construct_layout(self):
+        """ Used by child classes to construct the initial layout of the widget
+
+        Usually, the function should end with a self.setLayout() call.
+
+        Should only be used during init
+        """
+        raise NotImplementedError
+
+    @pyqtSlot()
+    def explanation(self):
+        """ Builds and shows a message box containing whatever text is set
+        on self.explain_text
+        """
+        return QMessageBox.question(
+            self, "Explanation", self.explain_text, QMessageBox.Ok,
+            QMessageBox.Ok
+        )
