1bd1788d52a780e3e3ff9b4160667c9221d0c28c
Daniel de Koning <daniel.de.koning@gmail.com>
2019-08-18 17:34:58 +0200

    Optimize some methods and add comments

--- activity_browser/app/ui/tables/history.py
+++ activity_browser/app/ui/tables/history.py
@@ -25,8 +25,8 @@ class ActivitiesHistoryTable(ABDataFrameView):
         super().__init__(parent=parent)
         self.setDragEnabled(True)
         self.setSelectionMode(QAbstractItemView.SingleSelection)
-        self._connect_signals()
         self.sync()
+        self._connect_signals()
 
     def _connect_signals(self):
         self.doubleClicked.connect(self.open_tab_event)
@@ -42,33 +42,34 @@ class ActivitiesHistoryTable(ABDataFrameView):
     def contextMenuEvent(self, a0):
         menu = QMenu(self)
         menu.addAction(
-            qicons.left, "Open in new tab", self.open_tab
+            qicons.right, "Open in new tab", self.open_tab
         )
         menu.popup(QCursor.pos())
         menu.exec()
 
     @pyqtSlot()
     def open_tab(self):
-        for proxy in self.selectedIndexes():
-            self.open_tab_event(proxy)
+        """ Only a single row can be selected for the history,
+        trigger the open_tab_event.
+        """
+        proxy = next(i for i in self.selectedIndexes())
+        self.open_tab_event(proxy)
 
     def open_tab_event(self, proxy):
         index = self.get_source_index(proxy)
-        row = self.dataframe.iloc[index.row(), ]
-        signals.open_activity_tab.emit(row["key"])
-        self.add_activity(row["key"])
+        key = self.dataframe.iloc[index.row(), ]["key"]
+        signals.open_activity_tab.emit(key)
+        self.add_activity(key)
 
     @pyqtSlot(tuple)
     def add_activity(self, key: tuple) -> None:
-        df = self.dataframe
-        row = df.loc[df["key"].isin([key])]
+        row = self.dataframe.loc[self.dataframe["key"].isin([key])]
 
         if not row.empty:
-            # Perform reindexing with existing data
-            df.drop(row.index, inplace=True)
-            df = pd.concat([row, df]).reset_index(drop=True)
+            # As data now exists in row, drop it from the dataframe
+            self.dataframe.drop(row.index, inplace=True)
         else:
-            # Build new row
+            # Data didn't exist, so build a new row with the key
             ds = bw.get_activity(key)
             data = {
                 self.HEADERS[i]: ds.get(self.COLUMNS[i], "")
@@ -78,9 +79,9 @@ class ActivitiesHistoryTable(ABDataFrameView):
             row = pd.DataFrame(
                 [data], index=[0], columns=self.HEADERS
             )
-            df = pd.concat([row, df]).reset_index(drop=True)
-        
-        self.sync(df)
+
+        # Rebuild model with dataframe, added activity is placed at start
+        self.sync(pd.concat([row, self.dataframe]).reset_index(drop=True))
 
     def _resize(self):
         self.setColumnHidden(4, True)  # Hide the 'key' column
