5a274ad752b7ab4f7856a727132cfd2e6a96a478
dgdekoning <daniel.de.koning@gmail.com>
2019-08-19 15:04:00 +0200

    Remove old MethodsTable code

--- activity_browser/app/ui/tables/impact_categories.py
+++ activity_browser/app/ui/tables/impact_categories.py
@@ -70,48 +70,6 @@ class MethodsTable(ABDataFrameView):
         ))
 
 
-class MethodsTable(ABTableWidget):
-    HEADERS = ["Name", "Unit", "# CFs"]
-    def __init__(self):
-        super(MethodsTable, self).__init__()
-        self.setDragEnabled(True)
-        self.setColumnCount(len(self.HEADERS))
-        self.setSizePolicy(QtWidgets.QSizePolicy(
-            QtWidgets.QSizePolicy.Preferred,
-            QtWidgets.QSizePolicy.Maximum)
-        )
-        self.connect_signals()
-        self.sync()
-
-    def connect_signals(self):
-        self.itemDoubleClicked.connect(
-            lambda x: signals.method_selected.emit(x.method)
-        )
-        signals.project_selected.connect(self.sync)
-
-    @ABTableWidget.decorated_sync
-    def sync(self, query=None):
-        self.setHorizontalHeaderLabels(self.HEADERS)
-
-        sorted_names = sorted([(", ".join(method), method) for method in bw.methods])
-
-        if query:
-            sorted_names = [
-                obj for obj in sorted_names
-                if query.lower() in obj[0].lower()
-            ]
-
-        self.setRowCount(len(sorted_names))
-
-        for row, method_obj in enumerate(sorted_names):
-            name, method = method_obj
-            data = bw.methods[method]
-            self.setItem(row, 0, ABTableItem(name, method=method))
-            self.setItem(row, 1, ABTableItem(data.get('unit', "Unknown"), method=method))
-            num_cfs = data.get('num_cfs', 0)
-            self.setItem(row, 2, ABTableItem(str(num_cfs), method=method, number=num_cfs, ))
-
-
 class CFTable(ABTableWidget):
     COLUMNS = {
         0: "name",
