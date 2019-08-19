cc9713be0140042df3040c42268d337294365b9d
dgdekoning <daniel.de.koning@gmail.com>
2019-08-19 15:02:44 +0200

    Implement MethodsTable from ABDataFrameView

--- activity_browser/app/ui/tables/impact_categories.py
+++ activity_browser/app/ui/tables/impact_categories.py
@@ -1,11 +1,73 @@
 # -*- coding: utf-8 -*-
 import numbers
-from PyQt5 import QtWidgets
 
 import brightway2 as bw
+import pandas as pd
+from PyQt5 import QtWidgets
 
-from . table import ABTableWidget, ABTableItem
-from ...signals import signals
+from activity_browser.app.signals import signals
+
+from .table import ABTableWidget, ABTableItem
+from .views import ABDataFrameView, dataframe_sync
+
+
+class MethodsTable(ABDataFrameView):
+    HEADERS = ["Name", "Unit", "# CFs", "method"]
+
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.drag_model = True
+        self.setDragEnabled(True)
+        self.setDragDropMode(ABDataFrameView.DragOnly)
+        self.sync()
+        self._connect_signals()
+
+    def _connect_signals(self):
+        self.doubleClicked.connect(self.method_selected)
+        signals.project_selected.connect(self.sync)
+
+    def method_selected(self, proxy):
+        index = self.get_source_index(proxy)
+        method = self.dataframe.iloc[index.row(), ]["method"]
+        signals.method_selected.emit(method)
+
+    def selectedItems(self) -> list:
+        """ Use to retrieve the method objects for the selected rows.
+
+        NOTE: Shadows the `selectedItems` method in QTableWidget as it is used
+        by `CSMethodsTable`. Possibly remove or improve in the future.
+        """
+        indexes = [self.get_source_index(p) for p in self.selectedIndexes()]
+        return [
+            self.dataframe.iloc[index.row(), ] for index in indexes
+        ]
+
+    @dataframe_sync
+    def sync(self, query=None):
+        sorted_names = sorted([(", ".join(method), method) for method in bw.methods])
+
+        if query:
+            sorted_names = filter(
+                lambda obj: query.lower() in obj[0].lower(), sorted_names
+            )
+
+        data = []
+        for method_obj in sorted_names:
+            method = bw.methods[method_obj[1]]
+            row =  {
+                "Name": method_obj[0],
+                "Unit": method.get("unit", "Unknown"),
+                "# CFs": str(method.get("num_cfs", 0)),
+                "method": method_obj[1],
+            }
+            data.append(row)
+        self.dataframe = pd.DataFrame(data, columns=self.HEADERS)
+
+    def _resize(self):
+        self.setColumnHidden(3, True)
+        self.setSizePolicy(QtWidgets.QSizePolicy(
+            QtWidgets.QSizePolicy.Preferred, QtWidgets.QSizePolicy.Maximum
+        ))
 
 
 class MethodsTable(ABTableWidget):
