394971c60b968aa18f2ead2840be44bce6c776c3
dgdekoning <daniel.de.koning@gmail.com>
2019-08-20 15:36:04 +0200

    Add delegate which present a boolean value as a checkbox

--- activity_browser/app/ui/tables/delegates.py
+++ activity_browser/app/ui/tables/delegates.py
@@ -2,7 +2,8 @@
 import brightway2 as bw
 from PyQt5.QtCore import QAbstractItemModel, QModelIndex, Qt
 from PyQt5.QtGui import QDoubleValidator
-from PyQt5.QtWidgets import QComboBox, QLineEdit, QStyledItemDelegate
+from PyQt5.QtWidgets import (QApplication, QComboBox, QLineEdit, QStyle,
+                             QStyledItemDelegate, QStyleOptionButton)
 
 
 class FloatDelegate(QStyledItemDelegate):
@@ -81,6 +82,29 @@ class DatabaseDelegate(QStyledItemDelegate):
         """
         value = editor.currentText()
         model.setData(index, value, Qt.EditRole)
+
+
+class CheckboxDelegate(QStyledItemDelegate):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+    def createEditor(self, parent, option, index):
+        return None
+
+    def paint(self, painter, option, index):
+        """ Paint the cell with a styled option button, showing a checkbox
+
+        See links below for inspiration:
+        https://stackoverflow.com/a/11778012
+        https://stackoverflow.com/q/15235273
+        """
+        value = bool(index.data(Qt.DisplayRole))
+        button = QStyleOptionButton()
+        button.state = QStyle.State_Enabled
+        button.state |= QStyle.State_Off if not value else QStyle.State_On
+        button.rect = option.rect
+        # button.text = "False" if not value else "True"  # This also adds text
+        QApplication.style().drawControl(QStyle.CE_CheckBox, button, painter)
 
 
 class ViewOnlyDelegate(QStyledItemDelegate):
