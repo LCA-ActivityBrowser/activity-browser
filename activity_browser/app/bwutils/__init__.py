8e50249fd5042a9f48931dfd83a889caf24e9f45
dgdekoning <daniel.de.koning@gmail.com>
2020-01-17 14:29:14 +0100

    Add additional classes for managing uncertainty logic
    and distributions

--- activity_browser/app/bwutils/__init__.py
+++ activity_browser/app/bwutils/__init__.py
@@ -7,7 +7,7 @@ import brightway2 as bw
 from .metadata import AB_metadata
 from .montecarlo import CSMonteCarloLCA
 from .multilca import MLCA, Contributions
-from .pedigree import PedigreeMatrix
+from .pedigree import DistributionGenerator, PedigreeMatrix, UncertainValues
 from .presamples import PresamplesContributions, PresamplesMLCA
 
 
--- activity_browser/app/bwutils/pedigree.py
+++ activity_browser/app/bwutils/pedigree.py
@@ -12,9 +12,12 @@ In turn, this CV can then be used to generate required values for a number of ot
 Any additions made should improve the transition of the calculated sigma (or Geometric Standard Deviation/GSD)
 smoothly into the related uncertainty distributions.
 """
+from collections import namedtuple
 import math
 from pprint import pformat
-from typing import Tuple
+
+import numpy as np
+from stats_arrays import uncertainty_choices as uc
 
 
 VERSION_2 = {
@@ -42,7 +45,7 @@ class PedigreeMatrix(object):
         self.factors = {}
 
     @classmethod
-    def from_numbers(cls, data: Tuple[int]) -> 'PedigreeMatrix':
+    def from_numbers(cls, data: tuple) -> 'PedigreeMatrix':
         """Takes a tuple of integers and construct a PedigreeMatrix.
         """
         assert len(data) in (5, 6), "Must provide either 5 or 6 factors"
@@ -67,3 +70,41 @@ class PedigreeMatrix(object):
 
     def __repr__(self) -> str:
         return "Empty Pedigree Matrix" if not self.factors else pformat(self.factors)
+
+
+UncertainValues = namedtuple("values", ("loc", "scale", "shape", "min", "max"))
+
+
+class DistributionGenerator(object):
+    """Generator class that is mostly here to have a central place to store
+    logic on which values are used in which distributions.
+
+    TODO: finish this at a later date.
+    """
+    @staticmethod
+    def generate_distribution(data: UncertainValues, dist_id: int, size: int = 1000) -> np.ndarray:
+        assert dist_id in uc.id_dict
+        if dist_id == 2:
+            return DistributionGenerator._log_normal(data, size)
+        elif dist_id == 3:
+            return DistributionGenerator._normal(data, size)
+
+    @staticmethod
+    def _log_normal(data: UncertainValues, size: int) -> np.ndarray:
+        assert not any(np.isnan([data.loc, data.scale]))
+        return np.random.lognormal(mean=data.loc, sigma=data.scale, size=size)
+
+    @staticmethod
+    def _normal(data: UncertainValues, size: int) -> np.ndarray:
+        assert not any(np.isnan([data.loc, data.scale]))
+        return np.random.normal(loc=data.loc, scale=data.scale, size=size)
+
+    @staticmethod
+    def _uniform(data: UncertainValues, size: int) -> np.ndarray:
+        assert not any(np.isnan([data.min, data.max]))
+        return np.random.uniform(low=data.min, high=data.max, size=size)
+
+    @staticmethod
+    def _triangular(data: UncertainValues, size: int) -> np.ndarray:
+        pass
+
