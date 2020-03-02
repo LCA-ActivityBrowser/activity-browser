99cb070033fc39669658bfb57510e727d77623d1
dgdekoning <daniel.de.koning@gmail.com>
2020-03-02 12:33:25 +0100

    Subclass float and uncertainty delegates to view-only

--- activity_browser/app/ui/tables/delegates/viewonly.py
+++ activity_browser/app/ui/tables/delegates/viewonly.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 from PySide2.QtWidgets import QStyledItemDelegate
 
+from .float import FloatDelegate
+from .uncertainty import UncertaintyDelegate
+
 
 class ViewOnlyDelegate(QStyledItemDelegate):
     """ Disable the editor functionality to allow specific columns of an
@@ -9,5 +12,17 @@ class ViewOnlyDelegate(QStyledItemDelegate):
     def __init__(self, parent=None):
         super().__init__(parent)
 
+    def createEditor(self, parent, option, index):
+        return None
+
+
+class ViewOnlyFloatDelegate(FloatDelegate):
+    """Correctly display float values without allowing modification."""
+    def createEditor(self, parent, option, index):
+        return None
+
+
+class ViewOnlyUncertaintyDelegate(UncertaintyDelegate):
+    """Correctly display uncertainty type without allowing modification."""
     def createEditor(self, parent, option, index):
         return None
