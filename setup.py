6bfc2f319dbdf048b96c0e139a34926ba59e8f57
dgdekoning <daniel.de.koning@gmail.com>
2020-08-31 14:55:49 +0200

    [skip ci] Version 2.6

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ for dirpath, dirnames, filenames in os.walk('activity_browser'):
 
 setup(
     name='activity-browser',
-    version="2.5.0",
+    version="2.6.0",
     packages=packages,
     include_package_data=True,
     author="Bernhard Steubing",
@@ -44,6 +44,7 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'Topic :: Scientific/Engineering :: Information Analysis',
         'Topic :: Scientific/Engineering :: Mathematics',
         'Topic :: Scientific/Engineering :: Visualization',
