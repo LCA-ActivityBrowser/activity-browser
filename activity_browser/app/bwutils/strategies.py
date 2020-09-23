0a9a561bfdb235242a5593aca8119ba484b2d433
dgdekoning <daniel.de.koning@gmail.com>
2020-09-23 17:46:17 +0200

    Add strategy for relinking an sqlite database

--- activity_browser/app/bwutils/strategies.py
+++ activity_browser/app/bwutils/strategies.py
@@ -2,7 +2,11 @@
 from typing import Collection
 
 import brightway2 as bw
-from bw2data.backends.peewee import ActivityDataset
+from bw2data.backends.peewee import ActivityDataset, sqlite3_lci_db
+from bw2data.errors import ValidityError
+from bw2io.errors import StrategyError
+from bw2io.strategies.generic import format_nonunique_key_error
+from bw2io.utils import DEFAULT_FIELDS, activity_hash
 
 
 def relink_exchanges_dbs(data: Collection, relink: dict) -> Collection:
@@ -37,3 +41,48 @@ def relink_exchanges_bw2package(data: dict, relink: dict) -> dict:
                     raise ValueError("Cannot relink exchange '{}', key '{}' not found.".format(exc, new_key)
                                      ).with_traceback(e.__traceback__)
     return data
+
+
+def relink_exchanges_existing_db(db: bw.Database, other: bw.Database) -> None:
+    """Relink exchanges after the database has been created/written.
+
+    This means possibly doing a lot of sqlite update calls.
+    """
+    assert db.backend == "sqlite", "Relinking only allowed for SQLITE backends"
+    assert other.backend == "sqlite", "Relinking only allowed for SQLITE backends"
+
+    duplicates, candidates = {}, {}
+    altered = 0
+
+    for ds in other:
+        key = activity_hash(ds, DEFAULT_FIELDS)
+        if key in candidates:
+            duplicates.setdefault(key, []).append(ds)
+        else:
+            candidates[key] = (ds['database'], ds['code'])
+
+    with sqlite3_lci_db.transaction() as transaction:
+        try:
+            # Only do relinking on external technosphere exchanges.
+            for i, exc in enumerate(
+                    exc for act in db for exc in act.technosphere()
+                    if exc.input[0] != db.name
+            ):
+                # Use the input activity to generate the hash.
+                key = activity_hash(exc.input, DEFAULT_FIELDS)
+                if key in duplicates:
+                    raise StrategyError(format_nonunique_key_error(exc.input, DEFAULT_FIELDS, duplicates[key]))
+                elif key in candidates:
+                    exc["input"] = candidates[key]
+                    altered += 1
+                exc.save()
+                if i % 10000 == 0:
+                    # Commit changes every 10k exchanges.
+                    transaction.commit()
+        except (StrategyError, ValidityError) as e:
+            print(e)
+            transaction.rollback()
+    # Process the database after the transaction is complete.
+    #  this updates the 'depends' in metadata
+    db.process()
+    print("Finished relinking database, {} exchanges altered.".format(altered))
