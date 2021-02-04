46430cda8907485fd6205bf6cc97845eff6db3ec
dgdekoning <daniel.de.koning@gmail.com>
2021-02-04 16:27:57 +0100

    Create ScenarioModel class

--- activity_browser/ui/tables/models/__init__.py
+++ activity_browser/ui/tables/models/__init__.py
@@ -5,4 +5,5 @@ from .base import (
 from .impact_categories import CFModel, MethodsListModel
 from .inventory import DatabasesModel, ActivitiesBiosphereModel
 from .lca_setup import CSActivityModel, CSMethodsModel, ScenarioImportModel
+from .scenarios import ScenarioModel
 from .tree import MethodsTreeModel, ParameterTreeModel
