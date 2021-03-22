f1b0b8ff21524bbbcbec73fe96b1e3d1a704d70c
dgdekoning <daniel.de.koning@gmail.com>
2021-03-22 14:00:17 +0100

    Remove imports from __init__.py

--- activity_browser/bwutils/presamples/__init__.py
+++ activity_browser/bwutils/presamples/__init__.py
@@ -3,6 +3,5 @@ from .manager import PresamplesParameterManager
 from .presamples_mlca import PresamplesContributions, PresamplesMLCA
 from .utils import (
     clear_resource_by_name, count_presample_packages, find_all_package_names,
-    get_package_path, load_scenarios_from_file, presamples_dir,
-    presamples_packages, remove_package, save_scenarios_to_file,
+    get_package_path, presamples_packages, remove_package
 )
