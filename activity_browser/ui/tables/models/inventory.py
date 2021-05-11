d0f1827dec422a3a80abc3e741eb528832a9266f
StpdFox <vosm4@cml.leidenuniv.nl>
2021-05-11 13:25:54 +0200

    changed string

--- activity_browser/ui/tables/models/inventory.py
+++ activity_browser/ui/tables/models/inventory.py
@@ -16,7 +16,7 @@ from .base import PandasModel, DragPandasModel
 
 
 class DatabasesModel(PandasModel):
-    HEADERS = ["Name", "Records", "Read-only", "Depends", "Modified"]
+    HEADERS = ["Name", "Records", "Read-only", "Depends", "Modiftied2"]
 
     def __init__(self, parent=None):
         super().__init__(parent=parent)
