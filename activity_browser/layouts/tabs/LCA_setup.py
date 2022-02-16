c248a3dd53920ca818c25966a5e54174e4677ccc
marc-vdm <m.t.van.der.meide@cml.leidenuniv.nl>
2022-02-16 21:26:18 +0100

    Insert splitter between reference flows and impact categories tables to allow user-resizing (fix for #532)

--- activity_browser/layouts/tabs/LCA_setup.py
+++ activity_browser/layouts/tabs/LCA_setup.py
@@ -146,16 +146,28 @@ class LCASetupTab(QtWidgets.QWidget):
         container.addLayout(calc_row)
         container.addWidget(horizontal_line())
 
-        self.ref_flow_header = header('Reference flows:')
-        self.impact_cat_header = header('Impact categories:')
-        self.middle_split = horizontal_line()
+        # widget for the reference flows
+        self.reference_flow_widget = QtWidgets.QWidget()
+        reference_flow_layout = QtWidgets.QVBoxLayout()
+        reference_flow_layout.addWidget(header('Reference flows:'))
+        reference_flow_layout.addWidget(self.activities_table)
+        self.reference_flow_widget.setLayout(reference_flow_layout)
+
+        # widget for the impact categories
+        self.impact_categories_widget = QtWidgets.QWidget()
+        impact_categories_layout = QtWidgets.QVBoxLayout()
+        impact_categories_layout.addWidget(header('Impact categories:'))
+        impact_categories_layout.addWidget(self.methods_table)
+        self.impact_categories_widget.setLayout(impact_categories_layout)
+
+        # splitter widget to combine the two above widgets
+        self.splitter = QtWidgets.QSplitter(Qt.Vertical)
+        self.splitter.addWidget(self.reference_flow_widget)
+        self.splitter.addWidget(self.impact_categories_widget)
+
         self.no_setup_label = QtWidgets.QLabel("To do an LCA, create a new calculation setup first by pressing 'New'.")
-        cs_panel_layout.addWidget(self.ref_flow_header)
-        cs_panel_layout.addWidget(self.activities_table)
-        cs_panel_layout.addWidget(self.middle_split)
-        cs_panel_layout.addWidget(self.impact_cat_header)
-        cs_panel_layout.addWidget(self.methods_table)
         cs_panel_layout.addWidget(self.no_setup_label)
+        cs_panel_layout.addWidget(self.splitter)
 
         self.cs_panel.setLayout(cs_panel_layout)
         container.addWidget(self.cs_panel)
@@ -296,12 +308,8 @@ class LCASetupTab(QtWidgets.QWidget):
                 self.presamples.list.setVisible(show)
                 self.presamples.remove.setVisible(show)
 
-        # show/hide other things
-        self.ref_flow_header.setVisible(show)
-        self.activities_table.setVisible(show)
-        self.middle_split.setVisible(show)
-        self.impact_cat_header.setVisible(show)
-        self.methods_table.setVisible(show)
+        # show/hide tables widgets
+        self.splitter.setVisible(show)
         self.no_setup_label.setVisible(not(show))
 
     @Slot(int, name="changeCalculationType")
