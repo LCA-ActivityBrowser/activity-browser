301a367b4157425e815625dae43d00d26125214e
haasad <haasad@users.noreply.github.com>
2022-03-11 17:12:56 +0100

    Disable GPU hardware acceleration for macOS Monterey

--- activity_browser/__init__.py
+++ activity_browser/__init__.py
@@ -14,12 +14,12 @@ from .info import __version__
 # https://bugreports.qt.io/browse/QTBUG-85546
 # https://github.com/mapeditor/tiled/issues/2845
 # https://doc.qt.io/qt-5/qoperatingsystemversion.html#MacOSBigSur-var
-if QSysInfo.productType() == "osx" and (
-    QSysInfo.productVersion() == "10.16" or QSysInfo.productVersion() == "11.0"
-):
-    os.environ["QT_MAC_WANTS_LAYER"] = "1"
-    os.environ["QTWEBENGINE_CHROMIUM_FLAGS"] = "--disable-gpu"
-    print("Warning! The currently used version of Qt cannot properly handle BigSur yet.")
+if QSysInfo.productType() == "osx":
+    supported = {'10.10', '10.11', '10.12', '10.13', '10.14', '10.15'}
+    if QSysInfo.productVersion() not in supported:
+        os.environ["QT_MAC_WANTS_LAYER"] = "1"
+        os.environ["QTWEBENGINE_CHROMIUM_FLAGS"] = "--disable-gpu"
+        print("Info: GPU hardware acceleration disabled")
 
 
 def run_activity_browser():
