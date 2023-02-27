68391dbe9e5454574bcf9bcf71c1bdd4fa0cf1b3
Zoophobus <jonathan.kidner@gmail.com>
2023-02-27 17:45:01 +0100

    Small changes to the styles (CSS) used in the AB, hopefully limiting the influence of external settings on the appearance of the AB

--- activity_browser/layouts/main.py
+++ activity_browser/layouts/main.py
@@ -37,6 +37,12 @@ class MainWindow(QtWidgets.QMainWindow):
         self.icon = qicons.ab
         self.setWindowIcon(self.icon)
 
+        self.setStyleSheet('''
+            QMainWindow { background-color: #f0f0f0; }
+            %QTabBar::tab:selected { background: #bababa; border-color: black; }
+            QTabWidget>QWidget>QWidget { background: white; }
+        ''')
+
         # Layout
         # The top level element is `central_widget`.
         # Inside is a vertical layout `vertical_container`.
