f14459b97795c5f742d4455903a8831fad3d65a2
Zoophobus <jonathan.kidner@gmail.com>
2023-04-19 16:36:43 +0200

    Update to the Errors used in the AB, currently mainly focused on managing imports for scenario files. Updates include: Adding an AB Error hierarchy, allowing certain sets of errors to be non-fatal; the addition of a few new Error types.

--- activity_browser/bwutils/errors.py
+++ activity_browser/bwutils/errors.py
@@ -1,44 +1,60 @@
 # -*- coding: utf-8 -*-
 
 
-class ImportCanceledError(Exception):
+class ABError(Exception):
+    """To be used as a generic Activity-Browser Error that will not lead to the AB crashing out"""
+
+
+class ABWarning(Warning):
+    """To be used as a generic Activity-Browser Warning"""
+
+class ImportCanceledError(ABError):
     """Import of data was cancelled by the user."""
     pass
 
 
-class LinkingFailed(Exception):
+class LinkingFailed(ABError):
     """Unlinked exchanges remain after relinking."""
     pass
 
 
-class IncompatibleDatabaseNamingError(Exception):
+class IncompatibleDatabaseNamingError(ABError):
     """Database and keys do not match."""
     pass
 
 
-class ActivityProductionValueError(Exception):
+class ActivityProductionValueError(ABError):
     """Production value for an activity == 0"""
     pass
 
 
-class InvalidSDFEntryValue(Exception):
+class InvalidSDFEntryValue(ABError):
     """NA values found for data type that cannot hold \"NA\"."""
     pass
 
 
-class ExchangeErrorValues(Exception):
+class ExchangeErrorValues(ABError):
     """In Brightway2 if there is an error in an exchange calculation the 'amount' field is not available for the
         Exchange"""
     pass
 
 
-class ReferenceFlowValueError(Warning):
+class ReferenceFlowValueError(ABWarning):
     """While a user can technically perform a calculation with the reference flows all set to 0, such a calculation
      makes no logical sense and will lead to downstream errors (due to 0 results)."""
     pass
 
 
-class DuplicatedScenarioExchangeWarning(Warning):
+class DuplicatedScenarioExchangeWarning(ABWarning):
     """Will warn the user that a loaded scenario table contains duplicate exchanges. Only the last added exchange value
     will be used."""
     pass
+
+class CriticalCalculationError(ABError):
+    """Should be raised if some action during the running of the calculation causes a critical Exception that will fail
+    the calculation. This is intended to be used with a Popup warning system that catches the original exception."""
+
+
+class CriticalScenarioExtensionError(ABError):
+    """Should be raised when combinging multiple scenario files by extension leads to zero scenario columns. Due to no
+    scenario columns being found in common between the scenario files."""
