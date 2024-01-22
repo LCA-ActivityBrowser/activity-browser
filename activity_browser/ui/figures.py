c064f3470a2ab47c7da605427d31e9bc8b582efd
marc-vdm <m.t.van.der.meide@cml.leidenuniv.nl>
2024-01-22 16:34:52 +0100

    Resolve issue with heatmap color scaling with negative results + add `%` sign to legend

--- activity_browser/ui/figures.py
+++ activity_browser/ui/figures.py
@@ -5,6 +5,7 @@ import brightway2 as bw
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg
 from matplotlib.figure import Figure
+import matplotlib.ticker as mtick
 import numpy as np
 import pandas as pd
 from PySide2 import QtWidgets
@@ -126,7 +127,7 @@ class LCAResultsPlot(Plot):
         # avoid figures getting too large horizontally
         dfp.index = [wrap_text(i, max_length=40) for i in dfp.index]
         dfp.columns = [wrap_text(i, max_length=20) for i in dfp.columns]
-        prop = dfp.divide(dfp.max(axis=0)).multiply(100)
+        prop = dfp.divide(dfp.abs().max(axis=0)).multiply(100)
         dfp.replace(np.nan, 0, inplace=True)
         if invert_plot:
             dfp = dfp.T
@@ -135,7 +136,8 @@ class LCAResultsPlot(Plot):
         sns.heatmap(
             prop, ax=self.ax, cmap="Blues", annot=dfp, linewidths=0.05,
             annot_kws={"size": 11 if dfp.shape[1] <= 8 else 9,
-                       "rotation": 0 if dfp.shape[1] <= 8 else 60}
+                       "rotation": 0 if dfp.shape[1] <= 8 else 60},
+            cbar_kws={'format': '%.0f%%'}
         )
         self.ax.tick_params(labelsize=8)
         if dfp.shape[1] > 5:
