0955df9ad3644251c9511f73a3998cdefc99df74
Marin Visscher <103424764+mrvisscher@users.noreply.github.com>
2024-02-06 14:34:38 +0100

    Supply parents to `QtWebEngine` objects (#1228)

--- activity_browser/ui/web/base.py
+++ activity_browser/ui/web/base.py
@@ -36,9 +36,9 @@ class BaseNavigatorWidget(QtWidgets.QWidget):
 
         # Setup JS / Qt interactions
         self.bridge = Bridge(self)
-        self.channel = QtWebChannel.QWebChannel()
+        self.channel = QtWebChannel.QWebChannel(self)
         self.channel.registerObject('bridge', self.bridge)
-        self.view = QtWebEngineWidgets.QWebEngineView()
+        self.view = QtWebEngineWidgets.QWebEngineView(self)
         self.view.loadFinished.connect(self.load_finished_handler)
         self.view.setContextMenuPolicy(Qt.PreventContextMenu)
         self.view.page().setWebChannel(self.channel)
