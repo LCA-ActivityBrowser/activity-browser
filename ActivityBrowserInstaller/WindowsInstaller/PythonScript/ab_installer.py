ae4b0840889db141aeeff01001843d63e6bf483d
Ruben Visser <rcjvisser@hotmail.com>
2024-05-03 13:55:56 +0200

    applied coding standard to the ab_installer.py

--- ActivityBrowserInstaller/WindowsInstaller/PythonScript/ab_installer.py
+++ ActivityBrowserInstaller/WindowsInstaller/PythonScript/ab_installer.py
@@ -1,3 +1,13 @@
+"""
+- ab_installer.py
+- Date of File Creation: 22/04/2024
+- Contributors: Thijs Groeneweg & Ruben Visser
+- Date and Author of Last Modification: 03/05/2024 - Ruben Visser
+- Synopsis of the File's purpose:
+    This Python script creates a directory named "ActivityBrowserEnvironment" and then extracts the
+    contents of the compressed tar file "ActivityBrowser.tar.gz" into that directory using the tar command.
+"""
+
 import os
 import subprocess
 
