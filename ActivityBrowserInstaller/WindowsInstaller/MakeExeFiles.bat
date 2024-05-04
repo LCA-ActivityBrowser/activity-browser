c5e49c224961e482d3b2b3c70f96d66146e5c7a9
ThijsGroeneweg <65073242+ThijsGroeneweg01@users.noreply.github.com>
2024-05-04 15:57:10 +0200

    run updater as admin
    We need to do this to make sure it can change files in the AB directory

--- ActivityBrowserInstaller/WindowsInstaller/MakeExeFiles.bat
+++ ActivityBrowserInstaller/WindowsInstaller/MakeExeFiles.bat
@@ -4,7 +4,6 @@ setlocal enabledelayedexpansion
 rem Ask user to input the directory containing Python script files
 set /p python_script_dir="Enter the directory containing Python script files: "
 
-rem Navigate to the specified directory
 cd /d "%python_script_dir%"
 
 rem Loop through all Python files in the directory
@@ -12,8 +11,13 @@ for %%f in (*.py) do (
     rem Extract file name without extension
     set "filename=%%~nf"
     
-    rem Run PyInstaller to create standalone executable with specified icon
-    pyinstaller --onefile --icon=icon.ico "%%f"
+    if "!filename!"=="Updater" (
+        rem Run PyInstaller to create standalone executable with specified icon and --uac-admin. The updater requires admin privileges to update the application files.
+        pyinstaller --onefile --icon=icon.ico --uac-admin "%%f"
+    ) else (
+        rem Run PyInstaller to create standalone executable with specified icon
+        pyinstaller --onefile --icon=icon.ico "%%f"
+    )
     
     rem Rename the output executable to the original filename
     ren "dist\%%~nxf.exe" "!filename!.exe"
