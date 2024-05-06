906ce933dde4ddef96e23cd27114a7130619c8ec
ThijsGroeneweg <65073242+ThijsGroeneweg01@users.noreply.github.com>
2024-05-06 12:38:46 +0200

    remove ActivityBrowser exe File

--- ActivityBrowserInstaller/WindowsInstaller/PythonScript/ab_uninstaller.py
+++ ActivityBrowserInstaller/WindowsInstaller/PythonScript/ab_uninstaller.py
@@ -2,7 +2,7 @@
 - ab_uninstaller.py
 - Date of File Creation: 22/04/2024
 - Contributors: Thijs Groeneweg & Ruben Visser
-- Date and Author of Last Modification: 03/05/2024 - Ruben Visser
+- Date and Author of Last Modification: 03/05/2024 - Thijs Groeneweg
 - Synopsis of the File's purpose:
     This Python script first obtains the current working directory and then constructs a path for a directory named
     "ActivityBrowserEnvironment" within that directory. It then attempts to remove this directory using shutil.rmtree().
@@ -12,12 +12,19 @@
 
 import shutil
 import os
+from ActivityBrowser import getActivityBrowserVersion
 
 currentDirectory = os.getcwd()
 directoryPath = os.path.join(currentDirectory, "ActivityBrowserEnvironment")
 
 try:
-    shutil.rmtree(directory_path)
+    shutil.rmtree(directoryPath)
     print(f"Directory '{directoryPath}' successfully removed.")
 except FileNotFoundError:
     print(f"Directory '{directoryPath}' not found.")
+
+try:
+    # Remove ActivityBrowser-<version> file
+    os.remove("ActivityBrowser-" + getActivityBrowserVersion())
+except FileNotFoundError:
+    print("ActivityBrowser file not found in the directory.")
