a5ec3028cf2c3ad52cbcaab533b8d7e8a4c1bb70
ThijsGroeneweg <65073242+ThijsGroeneweg01@users.noreply.github.com>
2024-05-10 10:45:35 +0200

    put helper function in the updater instead of the starter

--- ActivityBrowserInstaller/MacOSInstaller/PythonScript/ActivityBrowser Updater.py
+++ ActivityBrowserInstaller/MacOSInstaller/PythonScript/ActivityBrowser Updater.py
@@ -13,13 +13,13 @@
 import tempfile
 import threading
 import subprocess
-import shutil
+import re
+from packaging.version import parse
 import requests
 import os
 import sys
 from PyQt5.QtWidgets import QApplication, QLabel, QPushButton, QVBoxLayout, QHBoxLayout, QDialog, QDesktopWidget, QProgressBar
 from PyQt5.QtCore import QObject, pyqtSignal
-from ActivityBrowser import getLatestRelease, getActivityBrowserVersion, isSecondIputVersionNewer
 
 # Define constants
 INSTALLER_FILENAME = "activity-browser.app"
@@ -161,9 +161,9 @@ class updaterWindow(QDialog):
         layout.addWidget(self.messageLabel)
 
         # Check if we have the newest version of the Activity Browser installed.
-        currentVersion = getActivityBrowserVersion()
-        newestVersion = getLatestRelease(user="ThisIsSomeone", repo="activity-browser")
-        isOldVersion = isSecondIputVersionNewer(currentVersion, newestVersion)
+        currentVersion = self.getActivityBrowserVersion()
+        newestVersion = self.getLatestRelease(user="ThisIsSomeone", repo="activity-browser")
+        isOldVersion = self.isSecondIputVersionNewer(currentVersion, newestVersion)
 
         if not isOldVersion:
             # If not an old version, close the window
@@ -186,6 +186,62 @@ class updaterWindow(QDialog):
 
         self.setLayout(layout)
     
+    def getLatestRelease(self, user: str, repo: str) -> str:
+        """
+         Get the most recent version of the Activity Browser from the GitHub API.
+
+        Parameters:
+        - user (str): GitHub username of the repository owner.
+        - repo (str): Name of the GitHub repository.
+
+        Returns:
+        - str: The latest release version.
+        """
+        url = f"https://api.github.com/repos/{user}/{repo}/releases/latest"
+        try:
+            response = requests.get(url)
+            data = response.json()
+            return data['tag_name'] if 'tag_name' in data else None
+        # Handle exceptions that may occur during the request if the user is offline or the server is down.
+        except requests.exceptions.RequestException:
+            return None
+    
+    def getActivityBrowserVersion(self, directory: str = ".") -> str:
+        """
+            Get the version number of the ActivityBrowser file in the specified directory.
+
+            Parameters:
+            - directory (str): The directory to search for the ActivityBrowser file.
+
+            Returns:
+            - str: The version number of the ActivityBrowser file.
+            """
+        try:
+            for filename in os.listdir(directory):
+                match = re.match(r'ActivityBrowser-(\d+\.\d+\.\d+)', filename)
+                if match:
+                    return match.group(1)
+            print("ActivityBrowser file not found in the directory.")
+            return None
+        except FileNotFoundError:
+            print(f"Directory '{directory}' not found.")
+            return None
+
+    def isSecondIputVersionNewer(self, version1: str, version2: str) -> bool: 
+        """
+        Compare two version strings in the format 'X.Y.Z' and determine if the second version is newer than the first.
+
+        Parameters:
+        - version1 (str): The first version string to compare.
+        - version2 (str): The second version string to compare.
+
+        Returns:
+        - bool: True if version2 is newer than version1, False otherwise. Returns False if either version is None.
+        """
+        if version1 is None or version2 is None:
+            return False
+        return parse(version1) < parse(version2)
+
     def getActivityBrowserFilename(self) -> str:
         """
         Get the filename of the ActivityBrowser executable in the current directory.
