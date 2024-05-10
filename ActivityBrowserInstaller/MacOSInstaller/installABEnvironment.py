b6c45c17ed8ebc88bdd417eb37424c5ae9fc5dfe
ThijsGroeneweg <65073242+ThijsGroeneweg01@users.noreply.github.com>
2024-05-10 09:56:58 +0200

    Run all the shell comands in python

--- ActivityBrowserInstaller/MacOSInstaller/installABEnvironment.py
+++ ActivityBrowserInstaller/MacOSInstaller/installABEnvironment.py
@@ -1,13 +1,20 @@
 import subprocess
 
-def run_install_script():
+def installABEnvironMent():
+    """Install the 'ab' Conda environment with the 'activity-browser' package from conda-forge."""	
     try:
-        # Execute the shell script './install_ab.sh'
-        subprocess.run(['./install_ab.sh'], check=True)
+        # Initialize Conda in the shell session
+        subprocess.run(['conda', 'shell.bash', 'hook'], check=True)
+
+        # Check if the 'ab' environment exists
+        env_list = subprocess.run(['conda', 'env', 'list'], check=True, capture_output=True, text=True)
+        if "\bab\b" not in env_list.stdout:
+            # Create a Conda environment named 'ab' with 'activity-browser' package from conda-forge
+            subprocess.run(['conda', 'create', '-y', '-n', 'ab', '-c', 'conda-forge', 'activity-browser'], check=True)
     except subprocess.CalledProcessError as e:
         print(f"Error: {e}")
     except FileNotFoundError:
-        print("Error: The script './install_ab.sh' does not exist or is not accessible.")
+        print("Error: The command does not exist or is not accessible.")
 
 if __name__ == "__main__":
-    run_install_script()
+    installABEnvironMent()
