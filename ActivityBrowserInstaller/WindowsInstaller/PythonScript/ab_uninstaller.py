b61ff2b8eb53fb44de5b9e52ba656b503354290b
ThijsGroeneweg <65073242+ThijsGroeneweg01@users.noreply.github.com>
2024-05-13 11:41:01 +0200

    ab uninstaller for MAC
    changed the wrong file in previous commit

--- ActivityBrowserInstaller/WindowsInstaller/PythonScript/ab_uninstaller.py
+++ ActivityBrowserInstaller/WindowsInstaller/PythonScript/ab_uninstaller.py
@@ -2,7 +2,7 @@
 - ab_uninstaller.py
 - Date of File Creation: 22/04/2024
 - Contributors: Thijs Groeneweg & Ruben Visser
-- Date and Author of Last Modification: 13/05/2024 - Thijs Groeneweg
+- Date and Author of Last Modification: 03/05/2024 - Thijs Groeneweg
 - Synopsis of the File's purpose:
     This Python script first obtains the current working directory and then constructs a path for a directory named
     "ActivityBrowserEnvironment" within that directory. It then attempts to remove this directory using shutil.rmtree().
@@ -10,20 +10,32 @@
     If the directory is not found, it prints a message indicating that the directory was not found.
 """
 
-import subprocess
+import shutil
+import os
 
-def removeCondaEnv(env_name: str):
-    """
-    Remove a conda environment.
+def getActivityBrowserFilename() -> str:
+        """
+        Get the filename of the ActivityBrowser executable in the current directory.
 
-    Args:
-    - env_name: The name of the conda environment to remove.
-    """
-    try:
-        subprocess.run(["conda", "env", "remove", "--name", env_name], check=True)
-        print(f"Conda environment '{env_name}' successfully removed.")
-    except subprocess.CalledProcessError:
-        print(f"Failed to remove conda environment '{env_name}'.")
+        Returns:
+        - str: The filename of the ActivityBrowser executable.
+        """
+        for filename in os.listdir("."):
+            if filename.startswith("ActivityBrowser-"):
+                return filename
+        return None
 
 if __name__ == "__main__":
-    removeCondaEnv("ab")
+    currentDirectory = os.getcwd()
+    directoryPath = os.path.join(currentDirectory, "ActivityBrowserEnvironment")
+
+    try:
+        shutil.rmtree(directoryPath)
+        print(f"Directory '{directoryPath}' successfully removed.")
+    except FileNotFoundError:
+        print(f"Directory '{directoryPath}' not found.")
+
+    try:
+        os.remove(getActivityBrowserFilename())
+    except FileNotFoundError:
+        print("ActivityBrowser file not found in the directory.")
