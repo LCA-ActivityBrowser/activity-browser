a6ab810850c1a487d955e0439fc9d91716cef9c5
ThijsGroeneweg <65073242+ThijsGroeneweg01@users.noreply.github.com>
2024-05-17 13:18:28 +0200

    move files to combined

---
+++ ActivityBrowserInstaller/Combined/MakeExeFiles.bat
@@ -0,0 +1,28 @@
+@echo off
+setlocal enabledelayedexpansion
+
+rem Ask user to input the directory containing Python script files
+set /p python_script_dir="Enter the directory containing Python script files: "
+
+cd /d "%python_script_dir%"
+
+rem Loop through all Python files in the directory
+for %%f in (*.py) do (
+    rem Extract file name without extension
+    set "filename=%%~nf"
+    
+    if "!filename!"=="ActivityBrowser Updater" (
+        rem Run PyInstaller to create standalone executable with specified icon and --uac-admin. The updater requires admin privileges to update the application files.
+        pyinstaller --onefile --icon=icon.ico --uac-admin "%%f"
+    ) else (
+        rem Run PyInstaller to create standalone executable with specified icon
+        pyinstaller --onefile --icon=icon.ico "%%f"
+    )
+    
+    rem Rename the output executable to the original filename
+    ren "dist\%%~nxf.exe" "!filename!.exe"
+)
+
+rem Clean up temporary build files
+rmdir /s /q "build"
+del "*.spec"
