c06509789e63b77cfba79b1e8f087091e402de9b
BryanOwee <bryanowee2002@gmail.com>
2024-05-24 10:35:16 +0200

    Fix folder path for MAC compatability

--- ActivityBrowserInstaller/Combined/createApp.py
+++ ActivityBrowserInstaller/Combined/createApp.py
@@ -1,7 +1,7 @@
 import os
 import subprocess
 
-def build_macos_app(filename):
+def build_macos_app(filepath):
     try:
         # PyInstaller command with specified parameters
         command = [
@@ -9,19 +9,21 @@ def build_macos_app(filename):
             '-y',
             '--onefile',
             '--windowed',
-            '--name', filename,
-            '--add-data=./' + filename + ':.',
-            '--add-data=./openActivityBrowser.sh:.',
-            filename
+            '--name', os.path.splitext(os.path.basename(filepath))[0],
+            '--add-data', f'{filepath}:.',
+            '--add-data', './PythonScript/openActivityBrowser.sh:.',
+            filepath
         ]
 
         # Execute the PyInstaller command
         subprocess.run(command, check=True)
+        print(f"Successfully built {filepath}")
     except subprocess.CalledProcessError as e:
         print(f"Error: {e}")
 
 if __name__ == "__main__":
-    directory = '.'  # Replace with your directory
+    directory = '/Users/bryanowee/Documents/GitHub/SE2/activity-browser/ActivityBrowserInstaller/Combined/PythonScript'
     for filename in os.listdir(directory):
         if filename.endswith('.py'):
-            build_macos_app(filename)
+            full_path = os.path.join(directory, filename)
+            build_macos_app(full_path)
