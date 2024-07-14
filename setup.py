1ecd682723adb152d9486a5b0b868912e9f39224
William Flores <will7207@gmail.com>
2024-07-14 13:23:34 -0700

    fix: pypi publish workflow

--- setup.py
+++ setup.py
@@ -8,6 +8,9 @@ elif "PKG_VERSION" in os.environ:
     version = os.environ["PKG_VERSION"]
 else:
     version = os.environ.get("GIT_DESCRIBE_TAG", "0.0.0")
+    if "-" in version:
+        versions = version.split("-")
+        version = "{}.post{}".format(versions[0], versions[1])
 
 setup(
     version=version,
