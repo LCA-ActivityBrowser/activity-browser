0c75d6c788f57928f2faf1fb974aa529791c4246
Marin Visscher <103424764+mrvisscher@users.noreply.github.com>
2024-09-18 13:29:12 +0200

    Project selection through project menu (#1362)
    * Added open project menu to project menu
    
    * BW25 decorations
    
    * Added project name to AB title bar
    
    * Documentation
    
    * Quickfix for opening of Wiki

--- activity_browser/ui/menu_bar.py
+++ activity_browser/ui/menu_bar.py
@@ -1,3 +1,4 @@
+import os
 from PySide2 import QtGui, QtWidgets
 from PySide2.QtCore import QSize, QUrl, Slot
 
@@ -6,6 +7,8 @@ from activity_browser.mod import bw2data as bd
 
 from ..info import __version__ as ab_version
 from .icons import qicons
+
+AB_BW25 = True if os.environ.get("AB_BW25", False) else False
 
 
 class MenuBar(QtWidgets.QMenuBar):
@@ -46,6 +49,7 @@ class MenuBar(QtWidgets.QMenuBar):
 
     def setup_file_menu(self) -> None:
         """Build the menu for specific importing/export/updating actions."""
+        self.file_menu.addMenu(ProjectsMenu(self))
         self.file_menu.addAction(self.new_proj_action)
         self.file_menu.addAction(self.dup_proj_action)
         self.file_menu.addAction(self.delete_proj_action)
@@ -92,7 +96,7 @@ class MenuBar(QtWidgets.QMenuBar):
             "&About Qt", lambda: QtWidgets.QMessageBox.aboutQt(self.window)
         )
         self.help_menu.addAction(
-            qicons.question, "&Get help on the wiki", self.open_wiki()
+            qicons.question, "&Get help on the wiki", self.open_wiki
         )
         self.help_menu.addAction(
             qicons.issue, "&Report an idea/issue on GitHub", self.raise_issue_github
@@ -134,6 +138,47 @@ For license information please see the copyright on <a href="https://github.com/
         self.import_db_action.setEnabled(exists)
 
 
+class ProjectsMenu(QtWidgets.QMenu):
+    """
+    Menu that lists all the projects available through bw2data.projects
+    """
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setTitle("Open project")
+        self.populate()
+
+        self.aboutToShow.connect(self.populate)
+        self.triggered.connect(lambda act: bd.projects.set_current(act.text()))
+
+    def populate(self):
+        """
+        Populates the menu with the projects available in the database
+        """
+        import bw2data as bd
+
+        # clear the menu of any already existing actions
+        self.clear()
+
+        # sort projects alphabetically
+        sorted_projects = sorted(list(bd.projects))
+
+        # iterate over the sorted projects and add them as actions to the menu
+        for i, proj in enumerate(sorted_projects):
+            # check whether the project is BW25
+            bw_25 = (
+                False if not isinstance(proj.data, dict) else proj.data.get("25", False)
+            )
+
+            # add BW25 decorations if necessary
+            name = proj.name if not bw_25 or AB_BW25 else "[BW25] " + proj.name
+
+            # create the action and disable it if it's BW25 and BW25 is not supported
+            action = QtWidgets.QAction(name, self)
+            action.setEnabled(not bw_25 or AB_BW25)
+
+            self.addAction(action)
+
+
 class MigrationsMenu(QtWidgets.QMenu):
     def __init__(self, parent=None) -> None:
         super().__init__(parent)
@@ -142,6 +187,4 @@ class MigrationsMenu(QtWidgets.QMenu):
         self.install_migrations_action = actions.MigrationsInstall.get_QAction()
 
         self.addAction(self.install_migrations_action)
-
-
 
