b52cfd9167a0af98fa4e7fcafc9a98f4392abeed
Zoltan Szocs <zoltan.szocs@gmail.com>
2024-09-18 11:26:31 +0300

    ComboBoxDelegate can display now colored rows

--- activity_browser/ui/tables/delegates/combobox.py
+++ activity_browser/ui/tables/delegates/combobox.py
@@ -1,9 +1,12 @@
 from typing import Callable, Union, List, Tuple
 
 from PySide2 import QtWidgets
+from PySide2 import QtCore
 from PySide2.QtCore import Qt
+from PySide2.QtGui import QBrush
 
-ComboBoxItem = Union[str, Tuple[str, str]]
+ComboBoxItem = Union[str, Tuple[str, str], Tuple[str, str, QBrush]]
+
 
 class ComboBoxDelegate(QtWidgets.QStyledItemDelegate):
     """
@@ -12,9 +15,13 @@ class ComboBoxDelegate(QtWidgets.QStyledItemDelegate):
 
     def __init__(self, item_source: Union[List[ComboBoxItem], Callable[[], list[ComboBoxItem]]], parent=None):
         """
-        :param items: List of items to be shown in the combo box. Can be a callable
+        :param item_source: List of items to be shown in the combo box or a callable
                         returning the items, to allow the delegate to update the
                         list of items in the combobox.
+                        An item can be:
+                            - a string
+                            - a display string - value string pair
+                            - a display string, value string and text color (QBrush) triple
         """
         super(ComboBoxDelegate, self).__init__(parent)
         self.item_source = item_source  # List of items to be shown in the combo box
@@ -25,12 +32,21 @@ class ComboBoxDelegate(QtWidgets.QStyledItemDelegate):
         self._early_commit_item_text = item_text
 
     def createEditor(self, parent, option, index):
+        # Get them item list
         if callable(self.item_source):
-            items = self.item_source()
+            raw_items = self.item_source()
         else:
-            items = self.item_source
-        if isinstance(items[0], str):
-            items = [(item, item) for item in items]
+            raw_items = self.item_source
+        # Extract the colors, if provided
+        item_colors = [item[2] if len(item) == 3 else None for item in raw_items]
+        # Create the items in form of (str, str)
+        items: list[tuple[str, str]] = []
+        for item in raw_items:
+            if isinstance(item, str):
+                items.append((item, item))
+            else:
+                items.append((item[0], item[1]))
+
         self.item_values = [item[1] for item in items]
         editor = QtWidgets.QComboBox(parent)
         for item in items:
@@ -39,6 +55,10 @@ class ComboBoxDelegate(QtWidgets.QStyledItemDelegate):
                 item, userdata = item
             editor.addItem(item, userdata)
         editor.setCurrentIndex(self.item_values.index(index.data()))
+        # Set the colors for the entries
+        for i, brush in enumerate(item_colors):
+            if brush is not None:
+                editor.setItemData(i, brush, QtCore.Qt.ForegroundRole)
         if self._early_commit_item_text != "":
             editor.activated.connect(
                 lambda index: self._handle_activated(editor, index)
