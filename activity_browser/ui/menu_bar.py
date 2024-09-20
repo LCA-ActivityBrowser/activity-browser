09976afdefda5fb0a217e108665b060097b78432
Marin Visscher <m.r.visscher@cml.leidenuniv.nl>
2024-09-20 15:35:57 +0200

    Split Project Menu

--- activity_browser/ui/menu_bar.py
+++ activity_browser/ui/menu_bar.py
@@ -14,56 +14,21 @@ AB_BW25 = True if os.environ.get("AB_BW25", False) else False
 class MenuBar(QtWidgets.QMenuBar):
     def __init__(self, window):
         super().__init__(parent=window)
-        self.window = window
-        self.file_menu = QtWidgets.QMenu("&Project", self.window)
-        self.view_menu = QtWidgets.QMenu("&View", self.window)
-        self.windows_menu = QtWidgets.QMenu("&Windows", self.window)
-        self.tools_menu = QtWidgets.QMenu("&Tools", self.window)
-        self.help_menu = QtWidgets.QMenu("&Help", self.window)
+        self.view_menu = QtWidgets.QMenu("&View", self)
+        self.windows_menu = QtWidgets.QMenu("&Windows", self)
+        self.tools_menu = QtWidgets.QMenu("&Tools", self)
+        self.help_menu = QtWidgets.QMenu("&Help", self)
 
-        self.new_proj_action = actions.ProjectNew.get_QAction()
-        self.dup_proj_action = actions.ProjectDuplicate.get_QAction()
-        self.delete_proj_action = actions.ProjectDelete.get_QAction()
-        self.import_proj_action = actions.ProjectImport.get_QAction()
-        self.export_proj_action = actions.ProjectExport.get_QAction()
-        self.import_db_action = actions.DatabaseImport.get_QAction()
-        self.export_db_action = actions.DatabaseExport.get_QAction()
-        self.update_biosphere_action = actions.BiosphereUpdate.get_QAction()
-        self.manage_settings_action = actions.SettingsWizardOpen.get_QAction()
         self.manage_plugins_action = actions.PluginWizardOpen.get_QAction()
 
-        self.addMenu(self.file_menu)
+        self.addMenu(ProjectMenu(self))
         self.addMenu(self.view_menu)
         self.addMenu(self.tools_menu)
         self.addMenu(self.help_menu)
 
-        self.setup_file_menu()
         self.setup_view_menu()
         self.setup_tools_menu()
         self.setup_help_menu()
-        self.connect_signals()
-
-    def connect_signals(self):
-        bd.projects.current_changed.connect(self.biosphere_exists)
-        bd.databases.metadata_changed.connect(self.biosphere_exists)
-
-    def setup_file_menu(self) -> None:
-        """Build the menu for specific importing/export/updating actions."""
-        self.file_menu.addMenu(ProjectsMenu(self))
-        self.file_menu.addAction(self.new_proj_action)
-        self.file_menu.addAction(self.dup_proj_action)
-        self.file_menu.addAction(self.delete_proj_action)
-        self.file_menu.addSeparator()
-        self.file_menu.addAction(self.import_proj_action)
-        self.file_menu.addAction(self.export_proj_action)
-        self.file_menu.addSeparator()
-        self.file_menu.addAction(self.import_db_action)
-        self.file_menu.addAction(self.export_db_action)
-        self.file_menu.addAction(self.update_biosphere_action)
-        self.file_menu.addSeparator()
-        self.file_menu.addMenu(MigrationsMenu(self))
-        self.file_menu.addSeparator()
-        self.file_menu.addAction(self.manage_settings_action)
 
     def setup_view_menu(self) -> None:
         """Build the menu for viewing or hiding specific tabs"""
@@ -90,10 +55,10 @@ class MenuBar(QtWidgets.QMenuBar):
     def setup_help_menu(self) -> None:
         """Build the help menu for the menubar."""
         self.help_menu.addAction(
-            self.window.icon, "&About Activity Browser", self.about
+            self.window().icon, "&About Activity Browser", self.about
         )
         self.help_menu.addAction(
-            "&About Qt", lambda: QtWidgets.QMessageBox.aboutQt(self.window)
+            "&About Qt", lambda: QtWidgets.QMessageBox.aboutQt(self.parent())
         )
         self.help_menu.addAction(
             qicons.question, "&Get help on the wiki", self.open_wiki
@@ -110,11 +75,11 @@ All development happens on <a href="https://github.com/LCA-ActivityBrowser/activ
 For copyright information please see the copyright on <a href="https://github.com/LCA-ActivityBrowser/activity-browser/tree/main#copyright">this page</a>.<br><br>
 For license information please see the copyright on <a href="https://github.com/LCA-ActivityBrowser/activity-browser/blob/main/LICENSE.txt">this page</a>.<br><br>
 """
-        msgBox = QtWidgets.QMessageBox(parent=self.window)
+        msgBox = QtWidgets.QMessageBox(parent=self.parent())
         msgBox.setWindowTitle("About the Activity Browser")
-        pixmap = self.window.icon.pixmap(QSize(150, 150))
+        pixmap = self.parent().icon.pixmap(QSize(150, 150))
         msgBox.setIconPixmap(pixmap)
-        msgBox.setWindowIcon(self.window.icon)
+        msgBox.setWindowIcon(self.parent().icon)
         msgBox.setText(text.format(ab_version))
         msgBox.exec_()
 
@@ -130,7 +95,45 @@ For license information please see the copyright on <a href="https://github.com/
         )
         QtGui.QDesktopServices.openUrl(url)
 
-    @Slot(name="testBiosphereExists")
+
+class ProjectMenu(QtWidgets.QMenu):
+    def __init__(self, parent=None) -> None:
+        super().__init__(parent)
+
+        self.setTitle("Project")
+
+        self.new_proj_action = actions.ProjectNew.get_QAction()
+        self.dup_proj_action = actions.ProjectDuplicate.get_QAction()
+        self.delete_proj_action = actions.ProjectDelete.get_QAction()
+
+        self.import_proj_action = actions.ProjectImport.get_QAction()
+        self.export_proj_action = actions.ProjectExport.get_QAction()
+
+        self.import_db_action = actions.DatabaseImport.get_QAction()
+        self.export_db_action = actions.DatabaseExport.get_QAction()
+        self.update_biosphere_action = actions.BiosphereUpdate.get_QAction()
+
+        self.manage_settings_action = actions.SettingsWizardOpen.get_QAction()
+
+        self.addMenu(ProjectSelectionMenu(self))
+        self.addAction(self.new_proj_action)
+        self.addAction(self.dup_proj_action)
+        self.addAction(self.delete_proj_action)
+        self.addSeparator()
+        self.addAction(self.import_proj_action)
+        self.addAction(self.export_proj_action)
+        self.addSeparator()
+        self.addAction(self.import_db_action)
+        self.addAction(self.export_db_action)
+        self.addAction(self.update_biosphere_action)
+        self.addSeparator()
+        self.addMenu(MigrationsMenu(self))
+        self.addSeparator()
+        self.addAction(self.manage_settings_action)
+
+        bd.projects.current_changed.connect(self.biosphere_exists)
+        bd.databases.metadata_changed.connect(self.biosphere_exists)
+
     def biosphere_exists(self) -> None:
         """Test if the default biosphere exists as a database in the project"""
         exists = True if bd.config.biosphere in bd.databases else False
@@ -138,7 +141,7 @@ For license information please see the copyright on <a href="https://github.com/
         self.import_db_action.setEnabled(exists)
 
 
-class ProjectsMenu(QtWidgets.QMenu):
+class ProjectSelectionMenu(QtWidgets.QMenu):
     """
     Menu that lists all the projects available through bw2data.projects
     """
