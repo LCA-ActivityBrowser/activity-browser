b3a852fc77d647674fe419e469e23d5d5da3948e
Marin Visscher <m.r.visscher@cml.leidenuniv.nl>
2024-12-10 13:26:23 +0100

    Disabling default install tests because of deprecations

--- tests/actions/test_various_actions.py
+++ tests/actions/test_various_actions.py
@@ -17,74 +17,75 @@ from activity_browser.ui.widgets import (
 from activity_browser.ui.wizards.plugins_manager_wizard import PluginsManagerWizard
 from activity_browser.ui.wizards.settings_wizard import SettingsWizard
 
-
-@pytest.mark.skipif(
-    os.environ.get("TEST_FAST", False), reason="Skipped for faster testing"
-)
-def test_default_install(ab_app, monkeypatch, qtbot):
-    project_name = "biosphere_project"
-    projects.set_current(project_name)
-
-    monkeypatch.setattr(
-        EcoinventVersionDialog,
-        "exec_",
-        staticmethod(lambda *args, **kwargs: EcoinventVersionDialog.Accepted),
-    )
-    monkeypatch.setattr(
-        QtWidgets.QComboBox, "currentText", staticmethod(lambda *args, **kwargs: "3.9")
-    )
-
-    assert projects.current == project_name
-    assert "biosphere3" not in databases
-    assert not application.main_window.findChild(DefaultBiosphereDialog)
-
-    actions.DefaultInstall.run(check_patches=False)
-
-    dialog = application.main_window.findChild(DefaultBiosphereDialog)
-    with qtbot.waitSignal(dialog.finished, timeout=5 * 60 * 1000):
-        pass
-    # TODO: look into why AB_metadata not being populated
-    # qtbot.waitUntil(lambda: len(AB_metadata.dataframe) == 4709)
-
-    assert "biosphere3" in databases
-    assert len(Database("biosphere3")) == 4709
-    assert len(methods) == 762
-
-
-@pytest.mark.skipif(
-    os.environ.get("TEST_FAST", False), reason="Skipped for faster testing"
-)
-def test_biosphere_update(ab_app, monkeypatch, qtbot):
-    project_name = "biosphere_project"
-    projects.set_current(project_name)
-
-    monkeypatch.setattr(
-        QtWidgets.QMessageBox,
-        "question",
-        staticmethod(lambda *args, **kwargs: QtWidgets.QMessageBox.Ok),
-    )
-    monkeypatch.setattr(
-        EcoinventVersionDialog,
-        "exec_",
-        staticmethod(lambda *args, **kwargs: EcoinventVersionDialog.Accepted),
-    )
-    monkeypatch.setattr(
-        QtWidgets.QComboBox,
-        "currentText",
-        staticmethod(lambda *args, **kwargs: "3.9.1"),
-    )
-
-    assert projects.current == project_name
-    assert "biosphere3" in databases
-    assert len(Database("biosphere3")) == 4709
-
-    actions.BiosphereUpdate.run()
-
-    dialog = application.main_window.findChild(BiosphereUpdater)
-    with qtbot.waitSignal(dialog.finished, timeout=5 * 60 * 1000):
-        pass
-
-    assert len(Database("biosphere3")) == 4718
+####### DEPRECATED IN BW25
+#
+# @pytest.mark.skipif(
+#     os.environ.get("TEST_FAST", False), reason="Skipped for faster testing"
+# )
+# def test_default_install(ab_app, monkeypatch, qtbot):
+#     project_name = "biosphere_project"
+#     projects.set_current(project_name)
+#
+#     monkeypatch.setattr(
+#         EcoinventVersionDialog,
+#         "exec_",
+#         staticmethod(lambda *args, **kwargs: EcoinventVersionDialog.Accepted),
+#     )
+#     monkeypatch.setattr(
+#         QtWidgets.QComboBox, "currentText", staticmethod(lambda *args, **kwargs: "3.9")
+#     )
+#
+#     assert projects.current == project_name
+#     assert "biosphere3" not in databases
+#     assert not application.main_window.findChild(DefaultBiosphereDialog)
+#
+#     actions.DefaultInstall.run(check_patches=False)
+#
+#     dialog = application.main_window.findChild(DefaultBiosphereDialog)
+#     with qtbot.waitSignal(dialog.finished, timeout=5 * 60 * 1000):
+#         pass
+#     # TODO: look into why AB_metadata not being populated
+#     # qtbot.waitUntil(lambda: len(AB_metadata.dataframe) == 4709)
+#
+#     assert "biosphere3" in databases
+#     assert len(Database("biosphere3")) == 4709
+#     assert len(methods) == 762
+#
+#
+# @pytest.mark.skipif(
+#     os.environ.get("TEST_FAST", False), reason="Skipped for faster testing"
+# )
+# def test_biosphere_update(ab_app, monkeypatch, qtbot):
+#     project_name = "biosphere_project"
+#     projects.set_current(project_name)
+#
+#     monkeypatch.setattr(
+#         QtWidgets.QMessageBox,
+#         "question",
+#         staticmethod(lambda *args, **kwargs: QtWidgets.QMessageBox.Ok),
+#     )
+#     monkeypatch.setattr(
+#         EcoinventVersionDialog,
+#         "exec_",
+#         staticmethod(lambda *args, **kwargs: EcoinventVersionDialog.Accepted),
+#     )
+#     monkeypatch.setattr(
+#         QtWidgets.QComboBox,
+#         "currentText",
+#         staticmethod(lambda *args, **kwargs: "3.9.1"),
+#     )
+#
+#     assert projects.current == project_name
+#     assert "biosphere3" in databases
+#     assert len(Database("biosphere3")) == 4709
+#
+#     actions.BiosphereUpdate.run()
+#
+#     dialog = application.main_window.findChild(BiosphereUpdater)
+#     with qtbot.waitSignal(dialog.finished, timeout=5 * 60 * 1000):
+#         pass
+#
+#     assert len(Database("biosphere3")) == 4718
 
 
 def test_plugin_wizard_open(ab_app):
