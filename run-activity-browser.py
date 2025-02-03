8022743e6ec8ab5c04187b71c6fbbf729bfe2e1a
Marin Visscher <m.r.visscher@science.leidenuniv.nl>
2025-02-04 00:43:19 +0100

    Beta update notifier

--- run-activity-browser.py
+++ run-activity-browser.py
@@ -1,5 +1,22 @@
 # -*- coding: utf-8 -*-
-from activity_browser import run_activity_browser
+from activity_browser import run_activity_browser, version
+import requests
 
 if __name__ == '__main__':
+    url = "https://api.anaconda.org/package/mrvisscher/activity-browser-beta"
+    response = requests.get(url)
+
+    if response.status_code == 200 and version != "0.0.0" and version != response.json()['latest_version']:
+        print("Your Activity Browser Beta is outdated")
+        print("______________________________________")
+        print(f"Your version:     {version}")
+        print(f"Latest version:   {response.json()['latest_version']}")
+        print("______________________________________")
+        print("update by using `conda update activity-browser-beta`")
+        y = input("Press enter to exit...").lower().strip()
+        if y not in ["no", "i refuse", "denied", "please no"]:
+            exit()
+        if y == "please no":
+            print("How well mannered of you")
+
     run_activity_browser()
