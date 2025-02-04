a16b7fe24bc0ea86f5e5c92e62baa70e95bcba86
Marin Visscher <m.r.visscher@cml.leidenuniv.nl>
2025-02-04 13:50:53 +0100

    Activity browser auto update changes

--- run-activity-browser.py
+++ run-activity-browser.py
@@ -1,29 +1,62 @@
 # -*- coding: utf-8 -*-
-from importlib import reload
-import activity_browser as ab
+from importlib import metadata
+from os import environ
 import subprocess
 import requests
 
+
+def check_ab_update() -> bool:
+    ab_url = "https://api.anaconda.org/package/mrvisscher/activity-browser-beta"
+    ab_response = requests.get(ab_url)
+    ab_current = metadata.version("activity_browser")
+
+    if ab_response.status_code != 200:
+        print("Could not fetch latest activity browser beta version")
+        return False
+
+    print(f"activity_browser_beta: {ab_current} x {ab_response.json()['latest_version']}")
+
+    if ab_current == "0.0.0" or ab_current == ab_response.json()['latest_version']:
+        return False
+    return True
+
+
+def check_bf_update() -> bool:
+    bf_url = "https://api.anaconda.org/package/mrvisscher/bw_functional"
+    bf_response = requests.get(bf_url)
+    bf_current = metadata.version("bw_functional")
+    if bf_response.status_code != 200:
+        print("Could not fetch latest brightway functional version")
+        return False
+
+    print(f"bw_functional:         {bf_current} x {bf_response.json()['latest_version']}")
+
+    if bf_current == bf_response.json()['latest_version']:
+        return False
+    return True
+
+
+def run():
+    from activity_browser import run_activity_browser
+    print("Launching the Activity Browser")
+    run_activity_browser()
+
+
 if __name__ == '__main__':
-    url = "https://api.anaconda.org/package/mrvisscher/activity-browser-beta"
-    response = requests.get(url)
-
-    if response.status_code == 200:
-        print("Could not fetch latest beta version")
-
-    elif ab.version != "0.0.0" and ab.version != response.json()['latest_version']:
-        print("Your Activity Browser Beta is outdated")
-        print("______________________________________")
-        print(f"Your version:     {ab.version}")
-        print(f"Latest version:   {response.json()['latest_version']}")
-        print("______________________________________")
-        print("update by using `conda update activity-browser-beta`")
-        proc = subprocess.run(["conda", "update", "activity-browser-beta"])
-
+    print("Activity Browser 3 Beta Release")
+    print("______________________________________")
+    ab = check_ab_update()
+    bf = check_bf_update()
+    print("______________________________________")
+    if bf and environ.get("CONDA_DEFAULT_ENV"):
+        print("Updating bw_functional")
+        proc = subprocess.run(["conda", "update", "bw_functional"])
         if proc.returncode != 0:
             print("Update failed, try updating manually")
-        else:
-            reload(ab)
-            print(f"Update successful, launching ab {ab.version}")
+    if ab and environ.get("CONDA_DEFAULT_ENV"):
+        print("Updating activity-browser-beta")
+        proc = subprocess.run(["conda", "update", "activity-browser-beta"])
+        if proc.returncode != 0:
+            print("Update failed, try updating manually")
 
-    ab.run_activity_browser()
+    run()
