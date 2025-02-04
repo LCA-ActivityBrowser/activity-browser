39521d348a26b42a216db42db4a8452baecc6677
Marin Visscher <m.r.visscher@science.leidenuniv.nl>
2025-02-04 10:38:18 +0100

    Beta automatic updater

--- run-activity-browser.py
+++ run-activity-browser.py
@@ -1,22 +1,29 @@
 # -*- coding: utf-8 -*-
-from activity_browser import run_activity_browser, version
+from importlib import reload
+import activity_browser as ab
+import subprocess
 import requests
 
 if __name__ == '__main__':
     url = "https://api.anaconda.org/package/mrvisscher/activity-browser-beta"
     response = requests.get(url)
 
-    if response.status_code == 200 and version != "0.0.0" and version != response.json()['latest_version']:
+    if response.status_code == 200:
+        print("Could not fetch latest beta version")
+
+    elif ab.version != "0.0.0" and ab.version != response.json()['latest_version']:
         print("Your Activity Browser Beta is outdated")
         print("______________________________________")
-        print(f"Your version:     {version}")
+        print(f"Your version:     {ab.version}")
         print(f"Latest version:   {response.json()['latest_version']}")
         print("______________________________________")
         print("update by using `conda update activity-browser-beta`")
-        y = input("Press enter to exit...").lower().strip()
-        if y not in ["no", "i refuse", "denied", "please no"]:
-            exit()
-        if y == "please no":
-            print("How well mannered of you")
+        proc = subprocess.run(["conda", "update", "activity-browser-beta"])
 
-    run_activity_browser()
+        if proc.returncode != 0:
+            print("Update failed, try updating manually")
+        else:
+            reload(ab)
+            print(f"Update successful, launching ab {ab.version}")
+
+    ab.run_activity_browser()
