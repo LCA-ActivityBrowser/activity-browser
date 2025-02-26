b3f82af9b79728bcdd10599c04815bea331a8540
Marin Visscher <m.r.visscher@cml.leidenuniv.nl>
2025-02-26 15:49:46 +0100

    Update `parameter_rename` action

--- activity_browser/actions/parameter/parameter_rename.py
+++ activity_browser/actions/parameter/parameter_rename.py
@@ -1,14 +1,11 @@
-from typing import Any
-
 from qtpy import QtWidgets
+
+from bw2data.parameters import ParameterBase, parameters
 
 from activity_browser import application
 from activity_browser.actions.base import ABAction, exception_dialogs
-from bw2data.parameters import (ActivityParameter,
-                                                     DatabaseParameter,
-                                                     ProjectParameter,
-                                                     parameters)
 from activity_browser.ui.icons import qicons
+from activity_browser.bwutils import Parameter, refresh_parameter
 
 
 class ParameterRename(ABAction):
@@ -23,34 +20,25 @@ class ParameterRename(ABAction):
 
     @staticmethod
     @exception_dialogs
-    def run(parameter: Any):
+    def run(parameter: tuple | Parameter | ParameterBase, new_name: str = None):
+        parameter = refresh_parameter(parameter)
+
+        new_name = new_name or ParameterRename.get_new_name(parameter)
+
+        if not new_name:
+            return
+
+        getattr(parameters, f"rename_{parameter.param_type}_parameter")(
+                parameter.to_peewee_model(), new_name, update_dependencies=True
+            )
+
+    @staticmethod
+    def get_new_name(parameter: Parameter):
         new_name, ok = QtWidgets.QInputDialog.getText(
             application.main_window,
             "Rename parameter",
             f"Rename parameter '{parameter.name}' to:",
         )
 
-        if not ok or not new_name:
-            return
-
-        try:
-            if isinstance(parameter, ProjectParameter):
-                parameters.rename_project_parameter(
-                    parameter, new_name, update_dependencies=True
-                )
-            if isinstance(parameter, DatabaseParameter):
-                parameters.rename_database_parameter(
-                    parameter, new_name, update_dependencies=True
-                )
-            if isinstance(parameter, ActivityParameter):
-                parameters.rename_activity_parameter(
-                    parameter, new_name, update_dependencies=True
-                )
-        except Exception as e:
-            QtWidgets.QMessageBox.warning(
-                application.main_window,
-                "Could not save changes",
-                str(e),
-                QtWidgets.QMessageBox.Ok,
-                QtWidgets.QMessageBox.Ok,
-            )
+        if ok and new_name:
+            return new_name
