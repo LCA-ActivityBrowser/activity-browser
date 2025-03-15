13ec953685784b4392b3a1207c247877d1415977
Marin Visscher <m.r.visscher@cml.leidenuniv.nl>
2025-03-15 18:37:32 +0100

    Move calculation button

--- activity_browser/layouts/pages/calculation_setup/calculation_setup.py
+++ activity_browser/layouts/pages/calculation_setup/calculation_setup.py
@@ -2,7 +2,7 @@ from qtpy import QtWidgets
 
 import bw2data as bd
 
-from activity_browser import signals
+from activity_browser import signals, actions
 
 from .scenario_section import ScenarioSection
 from .functional_unit_section import FunctionalUnitSection
@@ -12,14 +12,14 @@ from .toolbar import Toolbar
 
 class CalculationSetupPage(QtWidgets.QWidget):
 
-    def __init__(self, calculation_setup_name: str, parent=None):
+    def __init__(self, cs_name: str, parent=None):
         super().__init__(parent)
 
-        self.calculation_setup_name = calculation_setup_name
+        self.calculation_setup_name = cs_name
 
-        self.toolbar = Toolbar(calculation_setup_name, self)
-        self.functional_unit_section = FunctionalUnitSection(calculation_setup_name, self)
-        self.impact_category_section = ImpactCategorySection(calculation_setup_name, self)
+        self.run_button = actions.CSCalculate.get_QButton(cs_name)
+        self.functional_unit_section = FunctionalUnitSection(cs_name, self)
+        self.impact_category_section = ImpactCategorySection(cs_name, self)
         self.scenario_section = ScenarioSection(self)
 
         # Build the layout of the widget
@@ -30,12 +30,16 @@ class CalculationSetupPage(QtWidgets.QWidget):
         Builds the layout of the widget.
         """
         layout = QtWidgets.QVBoxLayout()
-        layout.setContentsMargins(0, 0, 0, 0)
+        layout.setContentsMargins(0, 3, 0, 0)
 
-        layout.addWidget(self.toolbar)
+        top_layout = QtWidgets.QHBoxLayout()
+        top_layout.setContentsMargins(0, 0, 10, 0)
+        top_layout.addWidget(QtWidgets.QLabel("<b>⠀Functional Units:</b>"))
+        top_layout.addStretch()
+        top_layout.addWidget(self.run_button)
 
         # Add output label and view to the layout
-        layout.addWidget(QtWidgets.QLabel("<b>⠀Functional Units:</b>"))
+        layout.addLayout(top_layout)
         layout.addWidget(self.functional_unit_section)
 
         # Add input label and view to the layout
