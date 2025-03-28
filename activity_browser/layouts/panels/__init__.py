b725dacb25ac953d4c2da1139cf648912bb345de
Marin Visscher <m.r.visscher@cml.leidenuniv.nl>
2025-03-28 09:38:55 +0100

    Removed left panel

--- activity_browser/layouts/panels/__init__.py
+++ activity_browser/layouts/panels/__init__.py
@@ -1,11 +1,9 @@
 # -*- coding: utf-8 -*-
 from activity_browser.layouts.panels.panel import ABTab
-from activity_browser.layouts.panels.left import LeftPanel
 from activity_browser.layouts.panels.right import RightPanel
 
 
 __all__ = [
     "ABTab",
-    "LeftPanel",
     "RightPanel",
 ]
