8580d3a937c0bdc2d052e84854197ce047b664c7
Marin Visscher <m.r.visscher@cml.leidenuniv.nl>
2025-04-17 11:59:01 +0200

    Migrate to ABMenu in exchanges_tab.py

--- activity_browser/layouts/pages/activity_details/exchanges_tab.py
+++ activity_browser/layouts/pages/activity_details/exchanges_tab.py
@@ -206,30 +206,15 @@ class ExchangesView(widgets.ABTreeView):
     }
     hovered_item: "ExchangesItem" = None
 
-    class HeaderMenu(QtWidgets.QMenu):
-        """
-        A context menu for the header of the ExchangesView.
+    class HeaderMenu(widgets.ABMenu):
+        menuSetup = [
+            lambda m: m.setup_view_menu(),
+            lambda m: m.setup_allocation(),
+        ]
 
-        Attributes:
-            view_actions (list): The list of actions for the view menu.
-            set_alloc (QAction): The action to set the allocation.
-        """
-
-        def __init__(self, pos: QtCore.QPoint, view: "ExchangesView"):
-            """
-            Initializes the HeaderMenu.
-
-            Args:
-                pos: The position of the context menu.
-                view (ExchangesView): The view displaying the exchanges.
-            """
-            super().__init__(view)
-
-            model = view.model()
-
-            # Get the column index and name based on the position
-            col_index = view.columnAt(pos.x())
-            col_name = model.columns()[col_index]
+        def setup_view_menu(self):
+            table_view: ExchangesView = self.parent()
+            table_model: ExchangesModel = table_view.model()
 
             def toggle_slot(action: QtWidgets.QAction):
                 """
@@ -240,37 +225,34 @@ class ExchangesView(widgets.ABTreeView):
                 """
                 indices = action.data()
                 for index in indices:
-                    hidden = view.isColumnHidden(index)
-                    view.setColumnHidden(index, not hidden)
+                    hidden = table_view.isColumnHidden(index)
+                    table_view.setColumnHidden(index, not hidden)
 
             # Create the view menu
-            view_menu = QtWidgets.QMenu(view)
+            view_menu = QtWidgets.QMenu(table_view)
             view_menu.setTitle("View")
 
-            self.view_actions = []
             props_indices = []
 
             # Add actions for each column to the view menu
-            for i, col in enumerate(model.columns()):
+            for i, col in enumerate(table_model.columns()):
                 if col.startswith("property"):
                     props_indices.append(i)
                     continue
 
-                action = QtWidgets.QAction(model.columns()[i])
+                action = QtWidgets.QAction(table_model.columns()[i], self)
                 action.setCheckable(True)
-                action.setChecked(not view.isColumnHidden(i))
+                action.setChecked(not table_view.isColumnHidden(i))
                 action.setData([i])
-                self.view_actions.append(action)
 
                 view_menu.addAction(action)
 
             # Add a combined action for property columns
             if props_indices:
-                action = QtWidgets.QAction("properties")
+                action = QtWidgets.QAction("properties", self)
                 action.setCheckable(True)
-                action.setChecked(not view.isColumnHidden(props_indices[0]))
+                action.setChecked(not table_view.isColumnHidden(props_indices[0]))
                 action.setData(props_indices)
-                self.view_actions.append(action)
                 view_menu.addAction(action)
 
             # Connect the view menu actions to the toggle slot
@@ -279,51 +261,44 @@ class ExchangesView(widgets.ABTreeView):
             # Add the view menu to the context menu
             self.addMenu(view_menu)
 
-            # Add an allocation action if the column is a property
-            if col_name.startswith("property"):
-                self.set_alloc = actions.ActivityModify.get_QAction(view.activity.key, "allocation", col_name[9:])
-                self.set_alloc.setText(f"Allocate by {col_name[9:]}")
-                self.addAction(self.set_alloc)
+        def setup_allocation(self):
+            table_view: ExchangesView = self.parent()
 
-    class ContextMenu(QtWidgets.QMenu):
-        """
-        A context menu for the ExchangesView.
+            if database_is_locked(table_view.activity["database"]) or not self.column.startswith("property"):
+                return
 
-        Attributes:
-            add_product_action (QAction): The action to add a new product.
-            delete_exc_action (QAction): The action to delete an exchange.
-            exc_to_sdf_action (QAction): The action to copy the exchange to SDF.
-            remove_sub_action (QAction): The action to remove a substitute.
-        """
+            action = actions.ActivityModify.get_QAction(table_view.activity.key,
+                                                        "allocation",
+                                                        self.column[9:],
+                                                        parent=self)
+            action.setText(f"Allocate by {self.column[9:]}")
+            self.addAction(action)
 
-        def __init__(self, pos, view: "ExchangesView"):
-            """
-            Initializes the ContextMenu.
+        @property
+        def column(self):
+            view, model, pos = self.parent(), self.parent().model(), QtGui.QCursor.pos()
+            col_index = view.columnAt(view.mapFromGlobal(pos).x())
+            return model.columns()[col_index]
 
-            Args:
-                pos: The position of the context menu.
-                view (ExchangesView): The view displaying the exchanges.
-            """
-            super().__init__(view)
+    class ContextMenu(widgets.ABMenu):
+        menuSetup = [
+            lambda m: m.add(actions.ActivityNewProduct, [m.activity.key], enable=not m.locked),
+            lambda m: m.add(actions.ExchangeDelete, m.exchanges, enable=bool(m.exchanges) and not m.locked),
+            lambda m: m.add(actions.ExchangeSDFToClipboard, m.exchanges, enable=bool(m.exchanges)),
+        ]
 
-            # Add the action to add a new product
-            self.add_product_action = actions.ActivityNewProduct.get_QAction([view.activity.key])
-            self.addAction(self.add_product_action)
+        @property
+        def locked(self):
+            return database_is_locked(self.activity["database"])
 
-            index = view.indexAt(pos)
-            if index.isValid():
-                item: ExchangesItem = index.internalPointer()
+        @property
+        def activity(self):
+            return self.parent().activity
 
-                # Add actions for deleting an exchange and copying to SDF
-                self.delete_exc_action = actions.ExchangeDelete.get_QAction([item.exchange])
-                self.exc_to_sdf_action = actions.ExchangeSDFToClipboard.get_QAction([item.exchange])
-                self.addAction(self.delete_exc_action)
-                self.addAction(self.exc_to_sdf_action)
-
-                # Add action to remove a substitute if it exists
-                if not pd.isna(item["substitute"]):
-                    self.remove_sub_action = actions.FunctionSubstituteRemove.get_QAction(item.exchange.input)
-                    self.addAction(self.remove_sub_action)
+        @property
+        def exchanges(self):
+            indexes = self.parent().selectedIndexes()
+            return list(set(idx.internalPointer().exchange for idx in indexes if idx.isValid()))
 
     def __init__(self, parent):
         """
