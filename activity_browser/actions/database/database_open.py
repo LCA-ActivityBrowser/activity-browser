6749f8d16dd4b24c0df7906acc55a9976a303a32
Marin Visscher <m.r.visscher@cml.leidenuniv.nl>
2025-04-29 10:51:44 +0200

    Opening databases in a tabbed fashion

--- activity_browser/actions/database/database_open.py
+++ activity_browser/actions/database/database_open.py
@@ -1,6 +1,6 @@
 from logging import getLogger
 
-from qtpy.QtCore import Qt
+from qtpy.QtCore import Qt, QEventLoop
 
 from activity_browser import application
 from activity_browser.ui import widgets
@@ -17,9 +17,41 @@ class DatabaseOpen(ABAction):
     def run(database_names: list[str]):
         from activity_browser.layouts import panes
 
+        sibling = DatabaseOpen.find_sibling()
+
         for db_name in database_names:
             db_pane = panes.DatabaseProductsPane(application.main_window, db_name)
             dock_widget = widgets.ABDockWidget(db_name, application.main_window)
             dock_widget.setWidget(db_pane)
-            application.main_window.addDockWidget(Qt.DockWidgetArea.LeftDockWidgetArea, dock_widget)
+            application.main_window.addDockWidget(DatabaseOpen.get_area(), dock_widget)
 
+            if sibling:
+                application.main_window.tabifyDockWidget(sibling, dock_widget)
+
+                application.thread().eventDispatcher().processEvents(QEventLoop.ProcessEventsFlags.AllEvents)
+                dock_widget.raise_()
+
+    @staticmethod
+    def find_sibling():
+        """
+        Find the dockwidget location where the database pane should be opened.
+        """
+        from activity_browser.layouts import panes
+
+        all_dws = application.main_window.findChildren(widgets.ABDockWidget)
+        databases_dw = application.main_window.findChild(widgets.ABDockWidget, "dockwidget-databases_pane")
+
+        products_dws = [w for w in all_dws if
+                        isinstance(w.widget(), panes.DatabaseProductsPane) and
+                        application.main_window.dockWidgetArea(w) == application.main_window.dockWidgetArea(databases_dw) and
+                        not w.visibleRegion().isNull()
+                        ]
+        return products_dws[0] if products_dws else None
+
+    @staticmethod
+    def get_area():
+        """
+        Find the dockwidget location where the database pane should be opened.
+        """
+        databases_dw = application.main_window.findChild(widgets.ABDockWidget, "dockwidget-databases_pane")
+        return application.main_window.dockWidgetArea(databases_dw)
