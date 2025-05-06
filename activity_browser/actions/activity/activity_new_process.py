1f825b138e60d11d834e529289fea95cadac325e
Marin Visscher <m.r.visscher@cml.leidenuniv.nl>
2025-05-06 11:09:49 +0200

    Multi-backend support for `ActivityNewProcess`

--- activity_browser/actions/activity/activity_new_process.py
+++ activity_browser/actions/activity/activity_new_process.py
@@ -1,10 +1,10 @@
 from uuid import uuid4
 
 from qtpy.QtWidgets import QDialog
+import bw2data as bd
 
 from activity_browser import application
 from activity_browser.actions.base import ABAction, exception_dialogs
-from bw2data import Database, labels
 from activity_browser.ui.icons import qicons
 from activity_browser.ui.widgets.new_node_dialog import NewNodeDialog
 
@@ -35,24 +35,43 @@ class ActivityNewProcess(ABAction):
         if ref_product == "":
             ref_product = name
 
+        database = bd.Database(database_name)
+
+        if database.backend not in ["sqlite", "functional_sqlite"]:
+            raise ValueError("Database backend must be sqlite or functional_sqlite")
+
+        legacy_backend = database.backend == "sqlite"
+
         # create process
         new_proc_data = {
             "name": name,
             "location": location,
-            "type": "process",
+            "type": "process" if not legacy_backend else "processwithreferenceproduct",
         }
-        database = Database(database_name)
-        new_process = database.new_activity(code=uuid4().hex, **new_proc_data)
+
+        if legacy_backend:
+            new_proc_data["reference product"] = ref_product
+            new_proc_data["unit"] = unit
+
+        new_process: bd.Node = database.new_activity(code=uuid4().hex, **new_proc_data)
         new_process.save()
 
-        # create reference product
-        new_ref_prod_data = {
-            "name": ref_product,
-            "unit": unit,
-            "location": location,
-            "type": "product",
-        }
-        prod = new_process.new_product(code=uuid4().hex, **new_ref_prod_data)
-        prod.save()
+        if legacy_backend:
+            new_process.new_exchange(
+                input=new_process.key,
+                type="production",
+                amount=1.0,
+            ).save()
+
+        if not legacy_backend:
+            # create reference product
+            new_ref_prod_data = {
+                "name": ref_product,
+                "unit": unit,
+                "location": location,
+                "type": "product",
+            }
+            prod = new_process.new_product(code=uuid4().hex, **new_ref_prod_data)
+            prod.save()
 
         ActivityOpen.run([new_process.key])
