7c6db7073fe21ceb85f6b431a4a23b292e843573
Marin Visscher <m.r.visscher@cml.leidenuniv.nl>
2025-08-20 13:27:19 +0200

    Patch `typing` to fix Python 3.10 compat

--- activity_browser/__init__.py
+++ activity_browser/__init__.py
@@ -1,3 +1,11 @@
+import sys
+
+# This patch fixes a bug on Python 3.10 where shiboken6 patches the typing module with a broken Self type. Only way to
+# fix it is to patch it ourselves first.
+if sys.version_info[1] < 11:
+    import typing, typing_extensions
+    setattr(typing, "Self", typing_extensions.Self)
+
 try:
     import PySide6
 except ImportError:
