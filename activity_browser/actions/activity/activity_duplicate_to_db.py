f06d3a3a8ae3a74b10fdb47b80161d6442d9d6c5
Marin Visscher <m.r.visscher@cml.leidenuniv.nl>
2025-09-25 15:50:47 +0200

    Enable moving nodes to another database

--- activity_browser/actions/activity/activity_duplicate_to_db.py
+++ activity_browser/actions/activity/activity_duplicate_to_db.py
@@ -2,55 +2,56 @@ from typing import List
 
 from qtpy import QtWidgets
 
-from activity_browser import application, settings
+import bw2data as bd
+
+from activity_browser import application
+from activity_browser.bwutils import refresh_node
 from activity_browser.actions.base import ABAction, exception_dialogs
-from activity_browser.bwutils import commontasks
-from activity_browser.mod import bw2data as bd
 from activity_browser.ui.icons import qicons
 
 from .activity_open import ActivityOpen
 
 
 class ActivityDuplicateToDB(ABAction):
-    """
-    ABAction to duplicate an activity to another database. Asks the user to what database they want to copy the activity
-    to, returns if there are no valid databases or when the user cancels. Otherwise uses the activity controller to
-    duplicate the activities to the chosen database.
-    """
-
     icon = qicons.duplicate_to_other_database
     text = "Duplicate to other database"
 
     @classmethod
     @exception_dialogs
-    def run(cls, activity_keys: List[tuple], to_db: str = None):
-        # from activity_browser.bwutils.metadata import AB_metadata
-        activities = [bd.get_activity(key) for key in activity_keys]
+    def run(cls, nodes: List[tuple | int | bd.Node], to_db: str = None):
+        nodes = [refresh_node(node) for node in nodes]
+
+        dbs = {node.get("database") for node in nodes}
+        if not len(dbs) == 1:
+            raise ValueError("All selected activities must be from the same database.")
+        from_db = next(iter(dbs))
 
         if to_db and not cls.confirm_db(to_db):
             return
 
-        target_db = to_db or cls.request_db(activities)
+        to_db = to_db or cls.request_db(nodes, backend=bd.databases[from_db]["backend"])
 
-        if not target_db:
+        if not to_db:
             return
 
-        new_activity_keys = []
+        new_nodes = []
 
-        # otherwise move all supplied activities to the db using the controller
-        for activity in activities:
-            new_code = commontasks.generate_copy_code((target_db, activity["code"]))
-            new_activity = activity.copy(code=new_code, database=target_db)
-            new_activity_keys.append(new_activity.key)
+        # otherwise move all supplied nodes to the db by copying them
+        for node in nodes:
+            new_node = node.copy(database=to_db)
+            new_nodes.append(new_node)
 
-        ActivityOpen.run(new_activity_keys)
+        ActivityOpen.run(new_nodes)
 
     @staticmethod
-    def request_db(activities):
-        # get valid databases (not the original database, or locked databases)
-        origin_db = next(iter(activities)).get("database")
+    def request_db(nodes: list[bd.Node], backend: str) -> str | None:
+        # get valid databases (not the original database, locked databases, or databases with a different backend)
+        origin_db = next(iter(nodes)).get("database")
         target_dbs = [
-            db for db in settings.project_settings.get_editable_databases() if db != origin_db
+            db for db, meta in bd.databases.items() if
+            db != origin_db
+            and meta.get("read_only") is not True
+            and meta.get("backend") == backend
         ]
 
         # return if there are no valid databases to duplicate to
@@ -65,24 +66,20 @@ class ActivityDuplicateToDB(ABAction):
         # construct a dialog where the user can choose a database to duplicate to
         target_db, ok = QtWidgets.QInputDialog.getItem(
             application.main_window,
-            "Copy activity to database",
+            "Move node to database",
             "Target database:",
             target_dbs,
             0,
             False,
         )
 
-        # return if the user didn't choose, or canceled
-        if not ok:
-            return
-
-        return target_db
+        return target_db if ok else None
 
     @staticmethod
     def confirm_db(to_db: str):
         user_choice = QtWidgets.QMessageBox.question(
             application.main_window,
-            "Duplicate to new database",
-            f"Copy to {to_db} and open as new tab?",
+            "Move to new database",
+            f"Move to {to_db} and open as new tab?",
         )
         return user_choice == user_choice.Yes
